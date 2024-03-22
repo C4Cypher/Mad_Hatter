f94e351d901479c8c7163d806a69e3f9e3e8d83e
C4Cypher <c4cypher@gmail.com>
2024-03-22 00:37:48 -0400

    Trying to get my set utilities working

--- set_util.m
+++ set_util.m
@@ -16,6 +16,7 @@
 :- interface.
 
 :- import_module set.
+:- import_module list.
 
 :- pred nondet_insert(T, set(T), set(T)).
 :- mode nondet_insert(in, in, in) is semidet.
@@ -29,14 +30,17 @@
 :- mode nondet_remove(in, out, in) is det.
 :- mode nondet_remove(out, in, in) is nondet.
 
-:- pred multi_superset(set(T), set(T)).
-:- mode multi_superset(in, in) is semidet.
-:- mode multi_superset(in, out) is multi.
-
 :- pred multi_subset(set(T), set(T)).
 :- mode multi_subset(in, in) is semidet.
 :- mode multi_subset(out, in) is multi.
 
+:- pred multi_superset(set(T), set(T)).
+:- mode multi_superset(in, in) is semidet.
+:- mode multi_superset(in, out) is multi.
+
+:- pred list_subset(list(T), list(T)).
+:- mode list_subset(in, in) is semidet.
+:- mode list_subset(out, in) is multi.
 
 :- pred nondet_union(set(T), set(T), set(T)).
 :- mode nondet_union(in, in, in) is semidet.
@@ -45,11 +49,17 @@
 :- mode nondet_union(in, out, in) is nondet.
 :- mode nondet_union(out, in, out) is nondet.
 
+
+
 %-----------------------------------------------------------------------------%
 :- implementation.
 
+:- import_module multi_map.
+
 %-----------------------------------------------------------------------------%
-:- promise_equivalent_clauses(nondet_insert/3).
+:- pragma promise_equivalent_clauses(nondet_insert/3).
+
+nondet_insert(A::in, B::in, C::in) :- insert(A, B, C).
 
 nondet_insert(A::in, B::in, C::out) :- insert(A, B, C).
 
@@ -61,22 +71,32 @@ nondet_remove(A, B, C) :- nondet_insert(A, C, B).
 
 %-----------------------------------------------------------------------------%
 
-:- promise_equivalent_clauses(multi_superset/2)
+:- pragma promise_equivalent_clauses(multi_subset/2).
 
-multi_superset(A::in, B::in) :- superset(A, B).
+multi_subset(A::in, B::in) :- subset(A, B).
 
-multi_superset(A::in, B::out) :-
-	member(X, A), det_remove(X, A, Y), 
-	(B = Y ; multi_superset(Y, B) ).
+multi_subset(list_to_set(A)::out, B::in) :- list_subset(A, to_sorted_list(B)).
 
-multi_superset(init::in, init::out).
+multi_superset(A, B) :- multi_subset(B, A).
 
-multi_subset(A, B) :- multi_superset(B, A).
+%-----------------------------------------------------------------------------%
+ 
+list_subset([], []).
 
+list_subset([X | Ys], [X | Xs]) :- list_subset(Ys, Xs).
+
+list_subset(Ys, [_ | Xs]) :- list_subset(Ys, Xs).
+	
 
 %-----------------------------------------------------------------------------%
 
-:- promise_equivalent_clauses(nondet_union/3).
+:- pragma promise_equivalent_clauses(nondet_union/3).
 
 nondet_union(A::in, B::in, C::out) :- union(A, B, C).
+
+
+	% subunion(Left, Right, A, B) 
+:- pred subunion(set(T), set(T), set(T), set(T))
+
+
 	
