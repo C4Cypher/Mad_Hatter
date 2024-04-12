db38fe969d10fd902feb99d41cf7c4e2af417b31
C4Cypher <c4cypher@gmail.com>
2024-04-12 11:30:03 -0400

    further defining pred declarations

--- mh_declaration.m
+++ mh_declaration.m
@@ -16,11 +16,40 @@
 :- interface.
 
 :- import_module mh_symbol.
+:- import_module mh_type.
 :- import_module mh_term.
 :- import_module mh_expression.
+
+:- import_module list.
+:- import_module set.
 
 
 :- type declaration
 --->	type_def(symbol, mh_type)
 ;		state_var(predicate_signature)
-;		some [T] state_relation(predicate_signature, T =< relation_state(T)). 
+;		state_relation(predicate_signature,	(some[T] func = T::uo is det) ). 
+;		primitive_predicate(
+			predicate_signature, 
+			(pred(relation::in, relation::out) is nondet) )
+;		primitive_predicate(
+			predicate_signature,
+			(pred(relation::in, relation::out) is nondet),
+			promise_binds ).
+			
+/* 	promises that if the given elements of a relation are ground, 
+	the resulting elements of the output will be ground */
+	
+:- type binding_promise ---> binding_promise(set(int), set(int)).
+:- type binding_promises == set(binding_promise).
+
+:- func promise_bind(list(int), list(int)) = binding_promise.
+:- func pb(list(int), list(int)) = binding_promise. % pb is an abbriviation
+
+:- func promise_binds(list(binding_promise)) = binding_promises.
+
+:- implementation.
+
+promise_bind(From, To) = binding_promise(from_list(From), from_list(To)).
+pb(F, T) = promise_bind(F, T).
+
+promise_binds(List) = from_list(List).
