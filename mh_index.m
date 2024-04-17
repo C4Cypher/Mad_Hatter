5c11ee7db6477267f3eef4d56ae51579b8d0cac8
C4Cypher <c4cypher@gmail.com>
2024-04-17 11:20:29 -0400

    Added helper predicates for defining members of index(T)

--- mh_index.m
+++ mh_index.m
@@ -15,6 +15,7 @@
 
 :- interface.
 
+:- import_module int.
 :- import_module list.
 
 :- import_module array.
@@ -40,4 +41,85 @@
 % TODO: Subclass index with unique set modes 
 
 %-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+% Helper predicates for declaring instances of index/2
 
+:- pred valid_list_index(list(T), int).
+:- mode valid_list_index(in, in) is semidet.
+:- mode valid_list_index(in, out) is nondet. 
+
+:- pred list_index(list(T), int, T).
+:- mode list_index(in, in, in) is semidet.
+:- mode list_index(in, in, out) is semidet. 
+:- mode list_index(in, out, out) is nondet. 
+	
+:- pred set_list_index(int, T, list(T), list(T)).
+:- mode set_list_index(in, in, in, out) is semidet. 
+:- mode set_list_index(out, in, in, out) is nondet.
+
+%-----------------------------------------------------------------------------%
+
+:- pred valid_array_index(array(T), int).
+:- mode valid_array_index(in, in) is semidet.
+:- mode valid_array_index(in, out) is nondet. 
+
+:- pred array_index(array(T), int, T).
+:- mode array_index(in, in, in) is semidet.
+:- mode array_index(in, in, out) is semidet. 
+:- mode array_index(in, out, out) is nondet. 
+	
+:- pred set_array_index(int, T, array(T), array(T)).
+:- mode set_array_index(in, in, in, out) is semidet. 
+:- mode set_array_index(out, in, in, out) is nondet.
+	
+
+:- implementation.
+
+%-----------------------------------------------------------------------------%
+	
+valid_list_index([_], 0).
+
+valid_list_index([_ | Vs], I) :-
+	I > 0,
+	valid_list_index(Vs, I - 1).
+	
+
+
+:- pragma promise_equivalent_clauses(list_index/3).
+
+list_index(L::in, I::in, V::in) :- index0(L, I, V).
+
+list_index(L::in, I::in, V::out) :- index0(L, I, V). 
+		
+list_index([V | Vs]::in, I::out, U::out) :-
+	I = 0, V = U;
+	I > 0, list_index(Vs @ [_ | _], I - 1, U). 
+
+
+set_list_index(I, V, !L) :-
+	valid_list_index(!.L, I),
+	det_replace_nth(!.L, I + 1, V, !:L).
+
+%-----------------------------------------------------------------------------%
+
+:- pragma promise_equivalent_clauses(valid_array_index/2).
+
+valid_array_index(A::in, I::in) :- in_bounds(A, I).
+
+valid_array_index(A::in, I::out) :- all_ints_from_to(min(A), max(A), I).
+
+:- pred all_ints_from_to(int::in, int::in, int::out) is nondet.
+
+all_ints_from_to(From, To, Out) :-
+	From =< To,
+	( Out = From ; all_ints_from_to(From + 1, To, Out) ).
+
+array_index(A, I, V) :-
+	valid_array_index(A, I),
+	unsafe_lookup(A, I, V).
+	
+set_array_index(I, V, !A) :-
+	valid_array_index(!.A, I),
+	slow_set(I, V, !A).
+
+%-----------------------------------------------------------------------------%
