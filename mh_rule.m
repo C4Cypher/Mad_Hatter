0a19d9c9230f0ef0e9b6b3b854a55063b8b9a65f
C4Cypher <c4cypher@gmail.com>
2024-04-22 10:15:46 -0400

    Needed for lambadas

---
+++ mh_rule.m
@@ -0,0 +1,24 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2024 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_rule.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module mh_rule.
+
+:- interface.
+
+:- import_module mh_term.
+:- import_module mh_expression.
+
+:- type rule ---> rule(rule_head, rule_body).
+
+:- type rule_head 
+	--->	
