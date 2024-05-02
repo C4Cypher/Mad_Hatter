f137d6abea2cf1a30daaa7d0f95d759614ea1584
C4Cypher <c4cypher@gmail.com>
2024-05-02 11:14:09 -0400

    Using clause for rule bodies

---
+++ mh_clause.m
@@ -0,0 +1,23 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2024 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_clause.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module mh_clause.
+
+:- interface.
+
+:- import_module mh_term.
+:- import_module mh_symbol.
+:- import_module mh_relation.
+
+:- type clause 
+	--->	clause(
