0b8c351f977d97c6977b932de4f5d2aa2545e3fe
C4Cypher <c4cypher@gmail.com>
2024-05-12 12:55:19 -0400

    Starting to define declarations
    I want the process of defining this type to shape my eventual implementation of my module type, as modules will be constructed by application of declarations to transform them.
    
    In the process, I'm finding that I'm able to better shape and define my term and type systems as I go, it's touch and go.

--- mh_declaration.m
+++ mh_declaration.m
@@ -18,43 +18,15 @@
 :- import_module mh_symbol.
 :- import_module mh_type.
 :- import_module mh_term.
-:- import_module mh_expression.
 
 :- import_module list.
 :- import_module set.
 
 
 :- type declaration
---->	type_def(symbol, mh_type)
-;		type_def(symbol, list
-;		data_type(data_signature)
-;		state_var(predicate_signature)
-;		state_relation(predicate_signature,	   )
- 
-;		primitive_predicate( predicate_signature, 
-			(pred(relation::in, relation::out) is nondet) )
-			
-;		primitive_predicate( predicate_signature,
-			(pred(relation::in, relation::out) is nondet),
-			promise_binds ).
-
-;		primitive_function( function_signature,
-			(func(ground_relation) = mh_term is semidet) ).
-			
-/* 	promises that if the given elements of a relation are ground, 
-	the resulting elements of the output will be ground */
-	
-:- type binding_promise ---> binding_promise(set(int), set(int)).
-:- type binding_promises == set(binding_promise).
-
-:- func promise_bind(list(int), list(int)) = binding_promise.
-:- func pb(list(int), list(int)) = binding_promise. % pb is an abbriviation
-
-:- func promise_binds(list(binding_promise)) = binding_promises.
-
-:- implementation.
-
-promise_bind(From, To) = binding_promise(from_list(From), from_list(To)).
-pb(F, T) = promise_bind(F, T).
-
-promise_binds(List) = from_list(List).
+	--->	type_def(symbol, mh_type) 
+	;		type_def(symbol, list(mh_type_term), mh_type_term) %Parametric
+	; 		data_type(symbol)
+	;		data_type(symbol, list(mh_type))
+	;		primitive_type(symbol)
+	;		clause(mh_term).
