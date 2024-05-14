e0a4ec01b0c0fc7934ed0f7ed576a8df8fa47c06
C4Cypher <c4cypher@gmail.com>
2024-05-14 12:12:13 -0400

    Breaking up into superclasses

--- mh_primitive.m
+++ mh_primitive.m
@@ -15,15 +15,36 @@
 
 :- interface.
 
-:- import_module mh_symbol.
+
+:- import_module mh_parse_term.
+:- import_module mh_to_literal.
+:- import_module mh_term.
 
 :- import_module term.
 
 
-:- typeclass primitive(T) where [
-	pred parse_primitive(term, symbol, T),
-	mode parse_primitive(in, out, out) is semidet,
-	func primtive_to_literal(T) = string
+:- typeclass primitive(T) <= (parse_term(T), to_literal(T)) where [
+	pred primitive_type_name(T, string),
+	mode primitive_type_name(in, out) is det
 ].
 
+:- pred parse_primitive(term(_)::in, primitive::out) is det.
 
+:- func parse_primitive(term(_)) = primitive.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module mh_symbol.
+
+%-----------------------------------------------------------------------------%
+
+parse_primitive(Term, primitive(Symbol, Primitive) ) :-
+	parse_term(Term, Primitive),
+	primitive_type_name(Primitive, Name),
+	Symbol = symbol(Name).
+
+parse_primitive(Term) = PrimitiveTerm :- parse_primitive(Term, PrimitiveTerm).
+	
