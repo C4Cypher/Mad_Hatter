ed7b0f34f70bb20802b0c34280e4e9185951aed1
C4Cypher <c4cypher@gmail.com>
2024-05-20 15:50:10 -0400

    Need to finish symbol table

---
+++ mh_functor_table.m
@@ -0,0 +1,22 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2024 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_functor_table.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module functor_table.
+
+:- import_module map.
+:- import_module type_desc.
+:- import_module set.
+
+:- import_module mh_symbol.
+:- import_module mh_type.
+
