f3114dfe03f4cede209f922099f5e770568e1492
C4Cypher <c4cypher@gmail.com>
2024-05-20 15:49:40 -0400

    Still don't have enough yet to implment

--- mh_module.m
+++ mh_module.m
@@ -22,6 +22,47 @@
 :- import_module list.
 :- import_module set.
 
+%-----------------------------------------------------------------------------%
 
 :- type mh_module.
+
+	% init_module(Name, Module) true iff Module is a new module with name Name
+:- pred init_module(string, mh_module).
+:- mode init_module(in, in) is semidet.
+:- mode init_module(in, out) is det.
+:- mode init_module(in, uo) is det.
+
+:- func init_module(string) = mh_module.
+:- mode init_module(in) = out is det.
+:- mode init_module(in) = uo is det.
+
+:- pred is_init_module(mh_module::in) is semidet.
+
+:- func module_name(mh_module) = string.
+
+
+%-----------------------------------------------------------------------------%
+
+ % predicate to declare a primitive mercury type for a module
+:- pred add_primitive(string, type_desc, mh_module, mh_module).
+:- mode add_primitive(in, in, in, out).
+:- mode add_primitive(in, in, di, uo ).
+
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+
+
+%-----------------------------------------------------------------------------%
+
+:- type mh_module
+	--->	mh_module(functor_table, type_table).
+
+
+
+
+
 
