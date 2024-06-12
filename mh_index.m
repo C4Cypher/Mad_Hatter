c3e0d694779511717fa47b5943ebe948f3a16cd4
C4Cypher <c4cypher@gmail.com>
2024-06-12 08:48:00 -0400

    Default mode for index calls now det

--- mh_index.m
+++ mh_index.m
@@ -15,9 +15,15 @@
 
 :- interface.
 
+:- import_module mh_arity.
+
 %-----------------------------------------------------------------------------%
 
-:- typeclass index(T, U) <= (T -> U) where [
+% Types implementing index(T, U) will store values of type U in structures of
+% type T, indexed from 1 to arity(T), T must not have invalid indexes
+% between 1 and arity(T)
+
+:- typeclass index(T, U) <= ((T -> U), arity(T)) where [
 
 
 	pred valid_index(T, int), % valid_index(Container, Index)
@@ -28,24 +34,16 @@
 	% generate duplicate indexes, otherwise default_map/3 may produce
 	% undefined behavior.  This requirement allows higher order iterators
 	% to execute over valid indexes in place, rather than having to generate
-	% a list of solutions first
+	% a list of solutions first. My implementations will attempt to produce
+	% indexes from 1 to arity(T)
 
 	pred index(T, int, U),	% index(Container, Index, Value)
-	mode index(in, in, out) is semidet, % fail on invalid index
+	mode index(in, in, out) is det, % exception on invalid index
 	mode index(in, out, out) is nondet, % all indexed values
 	
 	pred set_index(int, U, T, T), % set_index(Index, Value, !Container)
-	mode set_index(in, in, in, out) is semidet, % fail on invalid index
+	mode set_index(in, in, in, out) is det, % exception on invalid index
 	mode set_index(out, in, in, out) is nondet, % update any index nondet
-	
-	pred index_size(T::in, int::out) is det,  % Number of valid indexes
-	
-	% Index bounds, fail if there are no valid indexes
-	pred max_index(T::in, int::out) is semidet,	% highest valid index
-	pred min_index(T::in, int::out) is semidet, % lowest valid index
-	
-	% true if there are no invalid indexes between min and max
-	pred consecutive_index(T::in) is semidet,
 	
 	% Iterate a closure over all values in a container with an accumulator
 	% in the same order as index/3
@@ -60,36 +58,33 @@
 %-----------------------------------------------------------------------------%
 % Index Operation determinism casts
 
-	% Throws an exception if index is out of range.
-
-:- pred det_index(T::in, int::in, U::out) is det <= index(T, U).
-
-:- pred det_set_index(int::in, U::in, T::in, T::out) is det <= index(T, U).
-
+	
 :- pred semidet_valid_index(T::in, int::in) is semidet <= index(T, _).
 :- pred nondet_valid_index(T::in, int::out) is nondet <= index(T, _).
+:- pred cc_nondet_valid_index(T::in, int::out) is cc_nondet <= index(T, _).
 
+:- pred det_index(T::in, int::in, U::out) is det <= index(T, U).
 :- pred semidet_index(T::in, int::in, U::out) is semidet <= index(T, U).
 :- pred nondet_index(T::in, int::out, U::out) is nondet <= index(T, U).
+:- pred cc_nondet_index(T::in, int::out, U::out) is cc_nondet <= index(T, U).
 
+:- pred det_set_index(int::in, U::in, T::in, T::out) is det <= index(T, U).
 :- pred semidet_set_index(int::in, U::in, T::in, T::out) is semidet 
 	<= index(T, U).
 :- pred nondet_set_index(int::out, U::in, T::in, T::out) is nondet 
+	<= index(T, U).
+:- pred cc_nondet_set_index(int::out, U::in, T::in, T::out) is cc_nondet 
 	<= index(T, U).
 
 %-----------------------------------------------------------------------------%
 % Function versions of index/2 methods
 
-:- func index(int, T) = U is semidet <= index(T, U).
-:- func 'index :='(int, T, U) = T is semidet <= index(T, U).
+:- func index(int, T) = U <= index(T, U).
+:- func 'index :='(int, T, U) = T <= index(T, U).
 
-:- func det_index(int, T) = U <= index(T, U).
-:- func 'det_index :='(int, T, U) = T <= index(T, U).
+:- func semidet_index(int, T) = U is semidet <= index(T, U).
+:- func 'semidet_index :='(int, T, U) = T is semidet <= index(T, U).
 
-:- func index_size(T) = int <= index(T, _).
-
-:- func max_index(T) = int is semidet <= index(T, _).
-:- func min_index(T) = int is semidet <= index(T, _).
 
 :- func fold_index(func(U, A) = A, T, A) = A <= index(T, U).
 :- func map_index(func(U) = U, T) = T <= index(T, U).
@@ -97,12 +92,12 @@
 %-----------------------------------------------------------------------------%
 % Validity checks
 
-% Succeeds if min_index(T) =< max_index(T) 
-:- pred valid_index_bounds_check(T::in) is semidet <= index(T, _).
+% Fails if arity is 0 and valid_index does not fail, arity is less than zero
+% or if arity is greater than zero and valid_index fails
+:- pred valid_index(T::in) is semidet <= index(T, _).
 
 % Fails if valid_index produces duplicate indexes
 :- pred valid_index_dup_check(T::in) is semidet <= index(T, _). 
-
 
 %-----------------------------------------------------------------------------%
 % Default implementation of index methods
@@ -112,18 +107,16 @@
 % valid_index/2 resulting in O(n) complexities, some instances
 % may have more efficient implementations availible.
 
-:- pred default_size(T::in, int::out) is det <= index(T, _).
-
-:- pred default_max(T::in, int::out) is semidet <= index(T, _).
-:- pred default_min(T::in, int::out) is semidet <= index(T, _).
-
-:- pred default_consecutive(T::in) is semidet <= index(T, _).
 
 :- pred default_fold(pred(U, A, A), T, A, A) <= index(T, U).
 :- mode default_fold(pred(in, in, out) is det, in, in, out) is det.
 	
 :- pred default_map(pred(U, U), T, T) <= index(T, U).
 :- mode default_map(pred(in, out) is det, 	in, out) is det.
+
+% index_out_of_range_error(Module, Pred, T, U, Index).
+:- pred index_out_of_range_error(string::in, string::in, T::unused, U::unused, 
+	int::in) is erroneous <= index(T, U).
 
 
 :- implementation.
@@ -140,45 +133,20 @@
 %-----------------------------------------------------------------------------%
 % Index Operation determinism casts
 
-det_index(T, I, U) :-
-	if valid_index(T, I) then (
-		if index(T, I, V) then U = V
-		else unexpected($module, $pred, 
-			"Index " ++ string(I) ++ 
-			" out of range for method call of index(" ++ 
-			type_name(type_of(T)) ++ ", " ++ type_name(type_of(U)) ++
-			"). mh_index.valid_index/2 should not have allowed this!")
-		)
-		else unexpected($module, $pred, 
-				"Index " ++ string(I) ++ 
-				" out of range for method call of index(" ++ 
-				type_name(type_of(T)) ++ ", " ++ type_name(type_of(U)) ++
-				").").
-				
-det_set_index(I, U, !T) :-
-	if valid_index(!.T, I) then (
-		if set_index(I, U, !.T, NewT) 
-			then !:T = NewT
-			else unexpected($module, $pred, 
-				"Index " ++ string(I) ++ 
-				" out of range for method call of index(" ++ 
-				type_name(type_of(!.T)) ++ ", " ++ type_name(type_of(U)) ++
-				"). mh_index.valid_index/2 should not have allowed this!")
-		)
-		else unexpected($module, $pred, 
-				"Index " ++ string(I) ++ 
-				" out of range for method call of index(" ++ 
-				type_name(type_of(!.T)) ++ ", " ++ type_name(type_of(U)) ++
-				").").
 
 semidet_valid_index(T, I) :- valid_index(T, I).
 nondet_valid_index(T, I) :- valid_index(T, I).
+cc_nondet_valid_index(T, I) :- valid_index(T, I).
 
-semidet_index(T, I, U) :- index(T, I, U).
-nondet_index(T, I, U) :- index(T, I, U).
+det_index(T, I, U) :- index(T, I, U).
+semidet_index(T, I, U) :- valid_index(T, I), index(T, I, U).
+nondet_index(T, I, U) :- valid_index(T, I),index(T, I, U).
+cc_nondet_index(T, I, U) :- valid_index(T, I),index(T, I, U).
 
-semidet_set_index(I, U, !T) :- set_index(I, U, !T).
-nondet_set_index(I, U, !T) :- set_index(I, U, !T).
+det_set_index(I, U, !T) :- set_index(I, U, !T).
+semidet_set_index(I, U, !T) :- valid_index(!.T, I),set_index(I, U, !T).
+nondet_set_index(I, U, !T) :- valid_index(!.T, I),set_index(I, U, !T).
+cc_nondet_set_index(I, U, !T) :- valid_index(!.T, I),set_index(I, U, !T).
 
 
 %-----------------------------------------------------------------------------%
@@ -188,14 +156,10 @@ T ^ index(I) = U :- index(T, I, U).
 
 (!.T ^ index(I) := U) = !:T :- set_index(I, U, !T).
 
-T ^ det_index(I) = U :- det_index(T, I, U).
+T ^ semidet_index(I) = U :- semidet_index(T, I, U).
 
-(!.T ^ det_index(I) := U) = !:T :- det_set_index(I, U, !T).
+(!.T ^ semidet_index(I) := U) = !:T :- semidet_set_index(I, U, !T).
 
-index_size(T) = I :- index_size(T, I).
-
-max_index(T) = M :- max_index(T, M).
-min_index(T) = M :- min_index(T, M).
 
 
 fold_index(Func, T, !.A) = !:A :- fold_index(function_closure(Func), T, !A).
@@ -215,9 +179,18 @@ function_closure(Func, !T) :- Func(!.T) = !:T.
 %-----------------------------------------------------------------------------%
 % Validity checks
 
-valid_index_bounds_check(T) :- min_index(T) =< max_index(T).
-
-
+valid_index(T) :- 
+	arity(T, Arity),
+	compare(ZeroComp, Arity, 0),
+	require_complete_switch [ZeroComp] (
+		ZeroComp = (<), fail
+	;	ZeroComp = (=), not valid_index(T, _)
+	;	ZeroComp = (>), not (
+			valid_index(T, I),
+			I < 1, 
+			I > Arity
+		)
+	).
 
 valid_index_dup_check(T) :-
 	promise_equivalent_solutions [DupSet]
@@ -240,62 +213,11 @@ dup_check(I, More, { !.Dups, !.Set }, { !:Dups, !:Set } ) :- (
 		insert(I, !Set),
 		More = yes
 	).
-		
-	
-	
 
 
 %-----------------------------------------------------------------------------%
 % Default implementation of index methods
 
-default_size(T, S) :- promise_equivalent_solutions [S] 
-	unsorted_aggregate(
-		nondet_valid_index(T),
-		aggregate_size, 0, S).
-
-:- pred aggregate_size(int::in, int::in, int::out) is det.
-
-aggregate_size(_, S, S+1).
-
-%-----------------------------------------------------------------------------%
-
-
-default_max(T, Max) :- 
-	M = yes(Max),
-	promise_equivalent_solutions [M] 
-		unsorted_aggregate(nondet_valid_index(T), aggregate_max, no, M).
-
-:- pred aggregate_max(int::in, maybe(int)::in, maybe(int)::out) is det.
-
-aggregate_max(I, no, yes(I)).
-aggregate_max(I, yes(M0), yes(M)) :- I < M0 -> M = I ; M = M0.  
-
-%-----------------------------------------------------------------------------%
-
-
-default_min(T, Min) :-
-	M = yes(Min),
-	promise_equivalent_solutions [M]
-		unsorted_aggregate(nondet_valid_index(T), aggregate_min, no, M).
-
-:- pred aggregate_min(int::in, maybe(int)::in, maybe(int)::out) is det.
-
-aggregate_min(I, no, yes(I)).
-aggregate_min(I, yes(M0), yes(M)) :- I > M0 -> M = I ; M = M0.  
-
-%-----------------------------------------------------------------------------%
-
-default_consecutive(T) :- 
-	contiguous(min_index(T), max_index(T), T).
-
-:- pred contiguous(int::in, int::in, T::in) is semidet <= index(T, _).
-
-contiguous(I, Max, T) :- 
-	valid_index(T, I),
-	(if I < Max then contiguous(I + 1, Max, T) else true).
-
-
-%-----------------------------------------------------------------------------%
 
 default_fold(Closure, T, !A) :-	promise_equivalent_solutions [!:A]
 	unsorted_aggregate(	
@@ -324,4 +246,8 @@ default_map_aggregator(Pred, I, !T) :-
 	Pred(U0, U),
 	det_set_index(I, U, !T).
 	
-	
+index_out_of_range_error(Module, Pred, T, U, I) :- unexpected(Module, Pred, 
+				"Index " ++ string(I) ++ 
+				" out of range for method call of index(" ++ 
+				type_name(type_of(T)) ++ ", " ++ type_name(type_of(U)) ++
+				").").
