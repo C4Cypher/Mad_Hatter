98ca25f4dba71f9b09164a24d72122f0e18ff6b5
C4Cypher <c4cypher@gmail.com>
2024-08-15 09:27:23 -0400

    changing interface for tuple(T) to mr_tuple(T)

--- mh_tuple.m
+++ mh_tuple.m
@@ -21,57 +21,65 @@
 :- import_module mh_arity.
 :- import_module mh_term.
 
-%-----------------------------------------------------------------------------%
-% Tuple typeclass
-
-% Represents an indexable tuple of terms
-
-:- typeclass tuple(T) <= arity(T) where [
-
-	% tuple_index(Tuple, Index, Term) 
-	% retreive the Term from the Tuple at Index, throw an exception if
-	% index is out of range, nondet version will fail if tuple is of zero arity
-	pred tuple_index(T, int, mh_term),
-	mode tuple_index(in, in, out) is det,
-	mode tuple_index(in, out, out) is nondet,
-	
-	% fold_tuple(Closure, Relation, !Accumulator)
-	% perform a left fold from index 1 to arity(Relation), calling Closure on
-	% each term in Relation with !Accumulator
-	pred fold_tuple(pred(mh_term, A, A), T, A, A),
-	mode fold_tuple(pred(in, in, out) is det, in, in, out) is det
-	
-].
-
-%-----------------------------------------------------------------------------%
-% Function versions and determinism casts of tuple methods
-
-:- func tuple_index(T, int) = mh_term <= tuple(T).
-
-:- func fold_tuple(func(mh_term, A) = A, T, A) = A <= tuple(T).
-
-:- pred det_tuple_index(T::in, int::in, mh_term::out) is det <= tuple(T).
-:- pred semidet_tuple_index(T::in, int::in, mh_term::out) is semidet 
-	<= tuple(T).
-:- pred nondet_tuple_index(T::in, int::out, mh_term::out) is nondet <= tuple(T).
-:- pred cc_nondet_tuple_index(T::in, int::out, mh_term::out) is cc_nondet 
-	<= tuple(T).
-	
 
 %-----------------------------------------------------------------------------%
 % Tuple type
 
+% Represents an indexable tuple of terms
+
 :- type mh_tuple
-	--->	some [T] mr_tuple(T) => tuple(T)
+	--->	some [T] mr_tuple(T) => mr_tuple(T)
 	;		list_tuple(list(mh_term))
 	;		array_tuple(array(mh_term)).
 
-:- func tuple(T) = mh_tuple <= tuple(T).
+:- func mr_tuple(T) = mh_tuple <= mr_tuple(T).
 :- mode tuple(in) = out is det.
 :- mode tuple(out) = in is semidet.
 
+:- pred tuple_size(mh_tuple::in, int::out) is det.
+:- func tuple_size(mh_tuple) = int.
+
 :- instance arity(mh_tuple).
-:- instance tuple(mh_tuple).
+:- instance mr_tuple(mh_tuple).
+
+
+%-----------------------------------------------------------------------------%
+% Tuple typeclass
+
+% A mercury type that can represent a tuple
+
+:- typeclass mr_tuple(T) <= arity(T) where [
+
+	% tuple_index(Tuple, Index, Term) 
+	% retreive the Term from the Tuple at Index, throw an exception if
+	% index is out of range, nondet version will fail if tuple is of zero arity
+	pred mr_tuple_index(T, int, mh_term),
+	mode mr_tuple_index(in, in, out) is det,
+	mode mr_tuple_index(in, out, out) is nondet,
+	
+	% fold_tuple(Closure, Relation, !Accumulator)
+	% perform a left fold from index 1 to arity(Relation), calling Closure on
+	% each term in Relation with !Accumulator
+	pred fold_mr_tuple(pred(mh_term, A, A), T, A, A),
+	mode fold_mr_tuple(pred(in, in, out) is det, in, in, out) is det
+	
+].
+
+%-----------------------------------------------------------------------------%
+% Function versions and determinism casts of tuple typeclass methods
+
+:- func mr_tuple_index(T, int) = mh_term <= mr_tuple(T).
+
+:- func fold_mr_tuple(func(mh_term, A) = A, T, A) = A <= mr_tuple(T).
+
+:- pred det_mr_tuple_index(T::in, int::in, mh_term::out) is det <= mr_tuple(T).
+:- pred semidet_mr_tuple_index(T::in, int::in, mh_term::out) is semidet 
+	<= mr_tuple(T).
+:- pred nondet_mr_tuple_index(T::in, int::out, mh_term::out) is nondet 
+	<= mr_tuple(T).
+:- pred cc_nondet_mr_tuple_index(T::in, int::out, mh_term::out) is cc_nondet 
+	<= mr_tuple(T).
+	
 	
 
 	
@@ -83,22 +91,7 @@
 
 :- import_module mh_index.
 
-%-----------------------------------------------------------------------------%
-% Function versions and determinism casts of tuple methods
 
-tuple_index(T, I) = Term :- tuple_index(T, I, Term).
-
-fold_tuple(Func, T, !.A) = !:A :- fold_tuple(function_closure(Func), T, !A).
-
-:- pred function_closure(func(T, A) = A, T, A, A).
-:- mode function_closure(func(in, in) = out is det, in, in, out) is det.
-
-function_closure(Func, T, !A) :- Func(T, !.A) = !:A.
-
-det_tuple_index(T, I, U) :- tuple_index(T, I, U).
-semidet_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
-nondet_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
-cc_nondet_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
 
 %-----------------------------------------------------------------------------%
 % Tuple type
@@ -125,6 +118,10 @@ tuple(T::out) = (Tuple::in) :-
 		Tuple = list_tuple(U), dynamic_cast(U, T);
 		Tuple = array_tuple(U), dynamic_cast(U, T)
 	).
+	
+tuple_size(mr_tuple(T), S) :- arity(T, S).
+tuple_size(list_tuple(L), S) :- length(L, S).
+tuple_size(array_tuple(Array), S) :- size(Array, S).
 
 
 :- instance arity(mh_tuple) where [
@@ -135,12 +132,30 @@ tuple(T::out) = (Tuple::in) :-
 
 :- instance tuple(mh_tuple) where [
 	
-	tuple_index(mr_tuple(T), I, Term) :- tuple_index(T, I, Term),
-	tuple_index(list_tuple(L), I, Term) :- list_index(L, I, Term),
-	tuple_index(array_tuple(A), I, Term) :- array_index(A, I, Term),
+	mr_tuple_index(mr_tuple(T), I, Term) :- tuple_index(T, I, Term),
+	mr_tuple_index(list_tuple(L), I, Term) :- list_index(L, I, Term),
+	mr_tuple_index(array_tuple(A), I, Term) :- array_index(A, I, Term),
 	
-	fold_tuple(Closure, mr_tuple(T), !A) :- fold_tuple(Closure, T, !A),
-	fold_tuple(Closure, list_tuple(L), !A) :- fold_list_index(Closure, L, !A),
-	fold_tuple(Closure, array_tuple(T), !A) :- fold_array_index(Closure, T, !A)
+	fold_mr_tuple(Closure, mr_tuple(T), !A) :- fold_tuple(Closure, T, !A),
+	fold_mr_tuple(Closure, list_tuple(L), !A) :- fold_list_index(Closure, L, !A),
+	fold_mr_tuple(Closure, array_mr_tuple(T), !A) :- 
+		fold_array_index(Closure, T, !A)
 ].
 
+%-----------------------------------------------------------------------------%
+% Function versions and determinism casts of tuple class methods
+
+mr_tuple_index(T, I) = Term :- mr_tuple_index(T, I, Term).
+
+fold_mr_tuple(Func, T, !.A) = !:A :- 
+	fold_mr_tuple(function_closure(Func), T, !A).
+
+:- pred function_closure(func(T, A) = A, T, A, A).
+:- mode function_closure(func(in, in) = out is det, in, in, out) is det.
+
+function_closure(Func, T, !A) :- Func(T, !.A) = !:A.
+
+det_mr_tuple_index(T, I, U) :- tuple_index(T, I, U).
+semidet_mr_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
+nondet_mr_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
+cc_nondet_mr_tuple_index(T, I, U) :- valid_index(T, I), tuple_index(T, I, U).
