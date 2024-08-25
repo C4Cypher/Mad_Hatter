5e87937350841de4a82fab06e156636cd4b786c9
C4Cypher <c4cypher@gmail.com>
2024-08-25 12:13:33 -0400

    Renamed cons, startred implementing sub coposition

--- mh_substitution.m
+++ mh_substitution.m
@@ -29,25 +29,23 @@
 	--->	sub_empty
 	;		sub_single(var_id, mh_term)
 	;		sub_array(array(mh_term))
-	;		sub_offset(array(mh_term), var_id_offset)
+	;		sub_array(array(mh_term), var_id_offset)
 	;		ren_empty
 	;		ren_single(var_id, var_id)
 	;		ren_array(array(var_id))
-	;		ren_offset(var_id_offset)
-	;		ren_offset(array(var_id), var_id_offset).
+	;		ren_array(array(var_id), var_id_offset)
+	;		ren_offset(var_id_offset, var_id_set).
 	
 
 	
 :- pred init_sub(mh_substitution::out) is det.
 :- func init_sub = mh_substitution.
 	
-:- pred empty_substitution(mh_substitution).
-:- mode empty_substitution(in) is semidet.
-:- mode empty_substitution(out) is multi.
+:- pred empty_substitution(mh_substitution::in) is semidet.
 
 % substitution_bounds(Subsitution, Min, Max)
 % Return the minimum and maximum var_id's indexed by Substitution, fail if the
-% Substituion is empty or a pure offset
+% Substituion is empty
 :- pred substitution_bounds(mh_substitution::in, var_id_offset::out, 
 	var_id_set::out) is semidet.
 
@@ -59,10 +57,6 @@
 % note that this will fail for any input with the ren_offset/1 constructor
 
 :- pred sub_contains_id(mh_substitution::in, var_id::in) is semidet.
-
-% Succeed if the substitution is an unbounded offset
-
-:- pred sub_unbounded(mh_substitution::in) is semidet.
 
 % Get the offset of a substitution, if it has one
 
@@ -136,7 +130,7 @@
 :- pred init_ren(mh_renaming::out) is det.
 :- func init_ren = mh_renaming.
 
-:- pred empty_renaming(mh_renaming).
+:- pred empty_renaming(mh_renaming::in) is semidet.
 :- mode empty_renaming(in) is semidet.
 :- mode empty_renaming(out) is cc_multi.
 
@@ -144,8 +138,6 @@
 % Looking up variables in renamings
 
 :- pred ren_contains_id(mh_renaming::in, var_id::in) is semidet.
-
-:- pred ren_unbounded(mh_renaming::in) is semidet.
 
 :- pred renaming_offset(mh_renaming::in, var_id_offset::out) is semidet.
 
@@ -198,26 +190,25 @@ init_sub = sub_empty.
 
 empty_substitution(sub_empty).
 empty_substitution(sub_array(make_empty_array)).
-empty_substitution(sub_offset(Sub, null_var_id_offset)) :- 
-	empty_substitution(Sub).
+empty_substitution(sub_array(make_empty_array, _)).
 empty_substitution(ren_empty).
 empty_substitution(ren_array(make_empty_array)).
-empty_substitution(ren_offset(null_var_id_offset)).
-empty_substitution(ren_offset(Ren, null_var_id_offset)) :-
-	empty_renaming(Ren).
+empty_substitution(ren_array(make_empty_array, _)).
+empty_substitution(ren_offset(null_var_id_offset, _)).
 	
 substitution_bounds(sub_single(ID, _), ID, ID).
 substitution_bounds(sub_array(Array), first_var_id, array_var_id_set(Array)).
-substitution_bounds(sub_offset(Array, Offset), 
-	first_var_id(Offset),
+substitution_bounds(sub_array(Array, Offset), Offset,
 	offset_array_var_id_set(Array, Offset)
 ).
 substitution_bounds(ren_single(ID, _), ID, ID).
 substitution_bounds(ren_array(Array), first_var_id, array_var_id_set(Array)).
-substitution_bounds(ren_offset(Array, Offset), 
-	first_var_id(Offset),
+substitution_bounds(ren_array(Array, Offset), Offset,
 	offset_array_var_id_set(Array, Offset)
 ).
+substitution_bounds(ren_offset(Offset, Set), Min, Set) :- 
+	Min = ( Offset =< null_var_id_offset -> null_var_id_offset ; Offset ).
+		
 
 %-----------------------------------------------------------------------------%
 % Looking up variables in substitutions
@@ -229,9 +220,10 @@ sub_contains_id(sub_array(Array), ID) :-
 	var_id_in_bounds(Array, ID),
 	not var_id_lookup(Array, ID, nil).
 
-sub_contains_id(sub_offset(Sub, Offset), ID1) :- 
-	var_id_offset(ID1, ID2, Offset), 
-	sub_contains_id(Sub, ID2).
+sub_contains_id(sub_array(Array, Offset), ID1) :- 
+	var_id_offset(ID1, ID2, Offset),
+	var_id_in_bounds(Array, ID2),
+	not var_id_lookup(Array, ID2, nil).
 	
 sub_contains_id(ren_empty, _) :- fail.
 sub_contains_id(ren_single(ID, _), ID).
@@ -240,17 +232,14 @@ sub_contains_id(ren_array(Array), ID1) :-
 	var_id_semidet_lookup(Array, ID1, ID2),
 	ID2 > 0.
 
-sub_contains_id(ren_offset(_), _) :- fail.
-
-sub_contains_id(ren_offset(Array, Offset), ID1) :- 
+sub_contains_id(ren_array(Array, Offset), ID1) :- 
 	var_id_offset(ID1, ID2, Offset), 
-	var_id_semidet_lookup(Array, ID1, ID2),
-	ID2 > 0.
+	var_id_semidet_lookup(Array, ID2, ID3),
+	ID3 > 0.
+	
+sub_contains_id(ren_offset(Offset, Set), ID) :- 
+	contains_var_id(Set, Offset, ID).
 
-sub_unbounded(ren_offset(_)).
-
-substitution_offset(sub_offset(_, Offset), Offset).
-substitution_offset(ren_offset(_, Offset), Offset).
 substitution_offset(ren_offset(Offset), Offset).
 
 %-----------------------------------------------------------------------------%
@@ -265,7 +254,7 @@ sub_id_search(sub_array(Array), ID, Term)  :-
 	var_id_semidet_lookup(Array, ID, Term),
 	Term \= nil.
 	
-sub_id_search(sub_offset(Array, Offset), ID1, Term) :-
+sub_id_search(sub_array(Array, Offset), ID1, Term) :-
 	var_id_semidet_lookup(Array, Offset, ID2, Term),
 	Term \= nil.
 	
@@ -277,12 +266,13 @@ sub_id_search(ren_array(Array), !.ID, var(!:ID)) :-
 	var_id_semidet_lookup(Array, !ID),
 	!:ID > 0.
 	
-sub_id_search(ren_offset(Offset), !.ID, var(!:ID) ) :- 
-	var_id_offset(!ID, Offset).
-	
-sub_id_search(ren_offset(Array, Offset), !.ID, var(!:ID)) :-
+sub_id_search(ren_array(Array, Offset), !.ID, var(!:ID)) :-
 	var_id_semidet_lookup(Array, Offset, ID2, Term),
 	!:ID > 0.
+	
+sub_id_search(ren_offset(Offset, Set), !.ID, var(!:ID) ) :- 
+	contains_var_id(Set, !.ID),
+	var_id_offset(!ID, Offset).
 
 sub_id_search(Sub, ID) = Term :- sub_id_search(Sub, ID, Term).
 
@@ -330,16 +320,122 @@ sub_quantified_lookup(Sub, Var) = Term :-
 
 :- pragma promise_equivalent_clauses(compose_substitutions/3).
 
-compose_substitutions(S, empty_substitution, S).
-compose_substitutions(empty_substitution, S, S).
+compose_substitutions(S, sub_empty, S).
+compose_substitutions(sub_empty, S, S).
+
+compose_substitutions(S, ren_empty, S).
+compose_substitutions(ren_empty, S, S).
 
 compose_substitutions(
-	sub_single(ID2, Term), 
-	sub_single(ID1, var(ID2)),
-	sub_single(ID1, Term)
-).
+	sub_single(ID2, Term2), 
+	sub_single(ID1, Term1),
+	Sub
+) :-
+	( if Term1 = var(ID2)
+	then
+		Sub = sub_single(ID1, Term2)
+	else 
+		compare(Comp, ID1, ID2),
+		(
+			Comp = (=),
+			Sub = sub_single(ID1, Term1)
+		;
+			(
+				Comp = (>),
+				first_var_id(Offset) = ID2,
+				last_var_id(Set) = ID1
+			;
+				Comp = (<),
+				first_var_id(Offset) = ID1,
+				last_var_id(Set) = ID2
+			),
+			var_id_set_init_array(Offset, Set, nil, !.Array),
+			var_id_set(ID1, Term1, Offset, !Array),
+			var_id_set(ID2, Term2, Offset, !Array),
+			( if Offset = null_var_id_offset
+			then Sub = sub_array(!:Array)
+			else Sub = sub_array(!:Array, Offset)
+			)
+		)
+		
+	).
+	
+compose_substitutions(
+	sub_single(ID2, Term2),
+	ren_single(ID1, Var1),
+	Sub
+) :- 
+	( if ID2 = Var1
+	then
+		Sub = sub_single(ID1, Term2)
+	else 
+		compare(Comp, ID1, ID2),
+		(
+			Comp = (=),
+			Sub = ren_single(ID1, Var1)
+		;
+			(
+				Comp = (>),
+				first_var_id(Offset) = ID2,
+				last_var_id(Set) = ID1
+			;
+				Comp = (<),
+				first_var_id(Offset) = ID1,
+				last_var_id(Set) = ID2
+			),
+			var_id_set_init_array(Offset, Set, nil, !.Array),
+			var_id_set(ID1, var(Var1), Offset, !Array),
+			var_id_set(ID2, Term2, Offset, !Array),
+			( if Offset = null_var_id_offset
+			then Sub = sub_array(!:Array)
+			else Sub = sub_array(!:Array, Offset)
+			)
+		)
+		
+	).
 
-compose_substitutions()
+compose_substiutions(
+	ren_single(ID2, Var2),
+	sub_single(ID1, Term1),
+	Sub
+) :-
+	( if Term1 = var(ID2)
+	then
+		Sub = ren_single(ID1, Var2)
+	else 
+		compare(Comp, ID1, ID2),
+		(
+			Comp = (=),
+			Sub = sub_single(ID1, Term1)
+		;
+			(
+				Comp = (>),
+				first_var_id(Offset) = ID2,
+				last_var_id(Set) = ID1
+			;
+				Comp = (<),
+				first_var_id(Offset) = ID1,
+				last_var_id(Set) = ID2
+			),
+			var_id_set_init_array(Offset, Set, nil, !.Array),
+			var_id_set(ID1, Term1, Offset, !Array),
+			var_id_set(ID2, var(Var2), Offset, !Array),
+			( if Offset = null_var_id_offset
+			then Sub = sub_array(!:Array)
+			else Sub = sub_array(!:Array, Offset)
+			)
+		)
+		
+	).
+
+compose_substitutions(
+	Sub2,
+	Sub1,
+	Sub3
+) :-
+	not (
+		Sub1, = 
+	)
 
 
 :- pred compose_substiution_step(
@@ -398,7 +494,7 @@ compose_substiution_step(
 is_renaming(ren_empty). 
 is_renaming(ren_single(_, _) ).
 is_renaming(ren_array(_) ).
-is_renaming(ren_offset(_) ).
+is_renaming(ren_array(_, _) ).
 is_renaming(ren_offset(_, _)).
 
 init_ren(init_ren).
@@ -406,8 +502,8 @@ init_ren = ren_empty.
 
 empty_renaming(ren_empty).
 empty_renaming(ren_array(make_empty_array)).
-empty_renaming(ren_offset(null_var_id_offset)).
-empty_renaming(ren_offset(make_empty_array, null_var_id_offset)).
+empty_renaming(ren_array(make_empty_array, _)).
+empty_renaming(ren_offset(null_var_id_offset, _)).
 
 
 %-----------------------------------------------------------------------------%
@@ -425,15 +521,15 @@ ren_contains_id(ren_array(Array), ID1) :-
 	var_id_semidet_lookup(Array, ID1, ID2).
 	ID2 > 0..
 
-ren_contains_id(ren_offset(Array, Offset), ID1) :- 
-	var_id_semidet_lookup(Array, Offset, ID1, ID2).
+ren_contains_id(ren_array(Array, Offset), ID1) :- 
+	var_id_semidet_lookup(Array, Offset, ID1, ID2),
 	ID2 > 0.
-
 	
-ren_unbounded(ren_offset(_)).
+ren_contains_id(ren_offset(Offset, Set), ID) :- 
+	contains_var_id(Set, Offset, ID).
 
-renaming_offset(ren_offset(_, Offset), Offset).
-renaming_offset(ren_offset(Offset), Offset).
+
+renaming_offset(ren_offset(Offset, _), Offset).
 
 %-----------------------------------------------------------------------------%
 
@@ -446,12 +542,14 @@ ren_id_search(ren_array(Array), !ID) :-
 	var_id_semidet_lookup(Array, !ID),
 	!:ID > 0.
 	
-ren_id_search(ren_offset(Offset), !ID) :- 
-	var_id_offset(!ID, Offset).
-	
-ren_id_search(ren_offset(Array, Offset), !ID) :-
+
+ren_id_search(ren_array(Array, Offset), !ID) :-
 	var_id_semidet_lookup(Array, Offset, ID2, Term),
 	!:ID > 0.
+	
+ren_id_search(ren_offset(Offset), !ID) :- 
+	contains_var_id(Set, !.ID),
+	var_id_offset(!ID, Offset).
 
 ren_id_search(Ren, !.ID) = !:ID :- ren_id_search(Ren, !ID).
 
