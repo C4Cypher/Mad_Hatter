a4f286c65dc705b8bb83ae6f617e9e4c43e8b2d6
C4Cypher <c4cypher@gmail.com>
2024-09-23 13:55:05 -0400

    Create mh_parrse.m

---
+++ mh_parrse.m
@@ -0,0 +1,17 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2023 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_parse.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module mh_parse.
+
+:- interface.
+
