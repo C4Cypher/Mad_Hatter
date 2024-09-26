3b41066f829428288322675e4b5e39ae15fe0b40
C4Cypher <c4cypher@gmail.com>
2024-09-26 14:27:30 -0400

    Update mh_clause.m

--- mh_clause.m
+++ mh_clause.m
@@ -16,68 +16,17 @@
 :- interface.
 
 :- import_module mh_term.
+:- import_module mh_predicate.
 
 %-----------------------------------------------------------------------------%
 % Clauses
 
 :- type mh_clause
-	--->	fact_clause(clause_head)
-	;		rule_clause(clause_head, mh_term).
+	--->	fact_clause(mh_term)
+	;		rule_clause(mh_term, mh_predicate).
 	
-:- func clause_head(mh_clause) = clause_head.
-
-:- inst mh_clause(I)
-	---> 	fact_clause(I)
-	;		rule_clause(I, ground).
-	
-:- inst fact_clause(I) ---> fact_clause(I).
-
-:- mode is_fact_clause(I) == ground >> fact_clause(I).
-
-:- inst fact_clause == fact_clause(ground).
-
-:- mode is_fact_clause == is_fact_clause(ground).
-
-	
-%-----------------------------------------------------------------------------%
-% Clause head
-
-:- type clause_head
-	--->	predicate_head(mh_term)				% X 	 :-
-	;		unification_head(mh_term, mh_term) 	% X = Y  :-
-	;		function_head(mh_term, mh_term).	% X -> Y :-
-
-:- inst predicate_head(I) ---> predicate_head(I).
-:- inst predicate_head == predicate_head(ground).
- 
-:- type predicate_head =< clause_head 
-	---> 	predicate_head(mh_term).
-
-:- inst unification_head(I) ---> unification_head(I, ground).
-:- inst unification_head == unification_head(ground).
-	
-:- type unification_head =< clause_head 
-	---> unification_head(mh_term, mh_term).
-	
-:- type function_head =< clause_head
-	---> function_head(mh_term, mh_term).
-	
-	
-%-----------------------------------------------------------------------------%
-% Clause subtypes
-
-:- type predicate_clause =< mh_clause
-	---> fact_clause(predicate_head)	% Head.
-	;	 rule_clause(predicate_head, mh_term).	% Head :- Body.
-	
-:- type unification_clause =< mh_clause
-	---> 	fact_clause(unification_head)	% X = Y.
-	;		rule_clause(unification_head, mh_term). % X = Y :- Body.
-	
-:- type function_clause =< mh_clause
-	--->	fact_clause(function_head) % X -> Y.
-	;		rule_clause(function_head, mh_term). % X -> Y :- Body.
-
+:- func clause_head(mh_clause) = mh_term.
+:- func clause_body(mh_clause) = mh_predicate.
 
 
 %-----------------------------------------------------------------------------%
@@ -90,4 +39,7 @@
 
 clause_head(fact_clause(Head)) = Head.
 clause_head(rule_clause(Head, _)) = Head.
+
+clause_body(fact_clause(_)) = true.
+clause_body(rule_clause(_, Body)) = Body.
 
