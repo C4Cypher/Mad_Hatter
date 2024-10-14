54d64b8560c5338e13c0c425908331e9be2d3650
C4Cypher <c4cypher@gmail.com>
2024-10-14 10:15:19 -0400

    restarting mh_module

--- mh_module.m
+++ mh_module.m
@@ -15,38 +15,51 @@
 
 :- interface.
 
-:- import_module mh_symbol.
-:- import_module mh_type.
-:- import_module mh_term.
 
-:- import_module list.
-:- import_module set.
+:- import_module type_desc.
+
+:- import_module mh_term.
+:- import_module mh_symbol.
+
+
 
 %-----------------------------------------------------------------------------%
+% Mad Hatter modules
 
 :- type mh_module.
 
-	% init_module(Name, Module) true iff Module is a new module with name Name
 :- pred init_module(string, mh_module).
-:- mode init_module(in, in) is semidet.
 :- mode init_module(in, out) is det.
-:- mode init_module(in, uo) is det.
 
 :- func init_module(string) = mh_module.
 :- mode init_module(in) = out is det.
-:- mode init_module(in) = uo is det.
-
-:- pred is_init_module(mh_module::in) is semidet.
 
 :- func module_name(mh_module) = string.
 
+%-----------------------------------------------------------------------------%
+% Module level atom substitution
+
+:- pred insert_atom(mh_symbol::in, mh_term::in, mh_module::in, mh_module::out)
+	is det.
+	
+:- func atom_lookup(mh_module, mh_symbol) = mh_term.
+:- pred atom_lookup(mh_module::in, mh_symbol::in, mh_term::out) is det.
+
+:- func atom_search(mh_module, mh_symbol) = mh_term is semidet.
+:- pred atom_search(mh_module::in, mh_symbol::in, mh_term::out) is semidet.
+
 
 %-----------------------------------------------------------------------------%
+% Mercury types
 
- % predicate to declare a primitive mercury type for a module
-:- pred add_primitive(string, type_desc, mh_module, mh_module).
-:- mode add_primitive(in, in, in, out).
-:- mode add_primitive(in, in, di, uo ).
+/*
+ % predicate to declare a valid mercury type for a module
+:- pred add_mr_type(mh_symbol, type_desc, mh_module, mh_module).
+:- mode add_mr_type(in, in, in, out).
+:- mode add_mr_type(in, in, di, uo ).
+*/
+
+%-----------------------------------------------------------------------------%
 
 
 %-----------------------------------------------------------------------------%
@@ -54,14 +67,47 @@
 
 :- implementation.
 
-:- import_module mh_arity.
+:- import_module map.
+:- import_module set.
+
 
 %-----------------------------------------------------------------------------%
+% Mad Hatter modules
 
-:- type functor_table == map(symbol_arity, 
+:- type symbol_map == map(mh_symbol, mh_term). 
+:- type mr_type_set == set(type_desc).
 
 :- type mh_module
-	--->	mh_module(functor_table, type_table).
+	--->	symbolic_module(
+				mh_symbol, 		% module name
+				symbol_map, 	% map of atoms to terms
+				mr_type_set		% set of mercury types to convert from terms
+				% declarations
+				% imported modules
+				% used modules
+			).
+			
+init_module(Name, init_module(Name)).
+
+init_module(Name) = symbolic_module(symbol(Name), map.init, set.init).
+
+module_name(symbolic_module(symbol(Name), _, _)) = Name.
+
+%-----------------------------------------------------------------------------%
+% Module level atom substitution
+
+insert_atom(Symbol, Term, 
+	symbolic_module(Name, !.Map, Types),
+	symbolic_module(Name, !:Map, Types)
+) :-
+	det_insert(Symbol, Term, !Map).
+	
+atom_lookup(symbolic_module(_, Map, _), Symbol) = map.lookup(Map, Symbol).
+atom_lookup(Module, Symbol, atom_lookup(Module, Symbol)).
+
+atom_search(symbolic_module(_, Map, _), Symbol) = map.search(Map, Symbol).
+atom_search(Module, Symbol, atom_search(Module, Symbol)).
+
 
 
 
