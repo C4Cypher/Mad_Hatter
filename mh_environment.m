6bbd02f2aba79d7fc9611dfd41d60a59fb802a96
C4Cypher <c4cypher@gmail.com>
2024-10-15 08:30:07 -0400

    Create mh_environment.m

---
+++ mh_environment.m
@@ -0,0 +1,54 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2024 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_environment.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module mh_environment.
+
+:- interface.
+
+:- import_module map.
+
+:- import_module mh_symbol.
+:- import_module mh_term.
+
+%-----------------------------------------------------------------------------%
+% Environment
+
+:- type mh_environment.
+
+:- func new_env(map(mh_symbol, mh_term)) = mh_environment.
+
+%-----------------------------------------------------------------------------%
+% Environment lookup
+
+:- pred lookup(mh_environment::in, mh_symbol::in, mh_term::out) is semidet.
+:- func lookup(mh_environment, mh_symbol) = mh_term is semidet.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+%-----------------------------------------------------------------------------%
+
+:- type mh_environment
+	--->	map_env(map(mh_symbol, mh_term)).
+	
+new_env(Map) = map_env(Map).
+	
+	
+%-----------------------------------------------------------------------------%
+% Environment lookup
+
+lookup(Env, Symb, lookup(Env, Symb)).
+
+lookup(map_env(Map), Symb) = map.search(Map, Symb). 
