62cfe0486016d58afcbf5d15ec624de0621140f1
C4Cypher <c4cypher@gmail.com>
2024-10-24 07:37:19 -0400

    Reverting to simpler symbol implementation

--- mh_symbol.m
+++ mh_symbol.m
@@ -22,6 +22,8 @@
 :- type mh_symbol.
 :- type mh_symbols == list(mh_symbol).
 
+:- inst mh_symbol(I) ---> ~I.
+
 
 
 :- func symbol(string) = mh_symbol.
@@ -52,73 +54,19 @@
 %-----------------------------------------------------------------------------%
 % Symbols
 
-:- type mh_symbol ---> s(symbol_ptr).
+:- type mh_symbol ---> ~string.
 
-:- pragma promise_equivalent_clauses(symbol/1).
-:- pragma inline(symbol/1).
+symbol(String) = '~'(String).
 
-symbol(String::in) = (s(Ptr)::out) :- construct_ptr(String, Ptr).
+:- pragma memo(symbol(in) = out).
 
-symbol(String::out) = (Symbol::in) :-
-	Symbol = s(Ptr),
-	promise_equivalent_solutions [String] deconstruct_ptr(Ptr, String).
 
 to_string(symbol(String)) = String.
 
-%-----------------------------------------------------------------------------%
-% Symbol pointers
-
-:- type symbol_ptr ---> ~string 
-	where equality is ptr_equality, comparison is ptr_comparison.
-
-:- pred construct_ptr(string::in, symbol_ptr::out) is det.
-
-:- pragma memo(construct_ptr/2).
-
-construct_ptr(S, '~'(S)).
-
-:- pred deconstruct_ptr(symbol_ptr::in, string::out) is cc_multi.
-
-:- pragma inline(deconstruct_ptr/2).
-
-deconstruct_ptr('~'(S), S).
-
-
-:- pred ptr_equality(symbol_ptr::in, symbol_ptr::in) is semidet.
-
-ptr_equality(A, B) :- private_builtin.pointer_equal(A, B).
-
-:- pragma inline(ptr_equality/2).
-
-:- pred ptr_comparison(comparison_result::uo, symbol_ptr::in, symbol_ptr::in) 
-	is det.
-	
-:- pragma inline(ptr_comparison/3).
-	
-ptr_comparison(R, A, B) :-
-	(if ptr_equality(A, B)
-	then R = (=)
-	else ptr_inequality(R, A, B)
-	).
-	
-:- pred ptr_inequality(comparison_result::uo,
-	symbol_ptr::in, symbol_ptr::in)	is det.
-	
-:- pragma inline(ptr_inequality/3).
-	
-ptr_inequality(R, A, B) :-
-	promise_equivalent_solutions [Astring, Bstring] (
-		deconstruct_ptr(A, Astring), 
-		deconstruct_ptr(B, Bstring)
-	),
-	(if compare((<), Astring, Bstring)
-	then R = (<)
-	else R = (>)
-	).
 
 %-----------------------------------------------------------------------------%
 % Symbol hashes
 
-symbol_hash(s(Ptr), cast_to_int(H)) :-
-	( promise_equivalent_solutions [String] deconstruct_ptr(Ptr, String) ),
-	fnv1a_hash(String, H).
+
+
+symbol_hash(symbol(String), cast_to_int(H)) :- fnv1a_hash(String, H).
