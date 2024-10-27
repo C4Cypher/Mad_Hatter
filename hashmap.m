aaad0211490be41bd76e42afefc027d49ff54bba
C4Cypher <c4cypher@gmail.com>
2024-10-27 08:56:05 -0400

    moved bit twiddling to bot, implemented two/5

--- hashmap.m
+++ hashmap.m
@@ -81,51 +81,6 @@ hash_size = bits_per_uint.
 	---> 	leaf(hash, K, V).
 	
 :- type leaf_array(K, V) == array(hashmap_leaf(K, V)).
-
-%-----------------------------------------------------------------------------%
-% Bit twiddling
-
-:- func bits_per_subkey = int.
-bits_per_subkey = 5.
-
-:- func maxchildren = int.
-maxchildren = unchecked_left_shift(1, bits_per_subkey).
-
-:- func subkey_mask = bitmap.
-subkey_mask = unchecked_right_shift(1, bits_per_subkey - 1).
-
-:- func index(bitmap, shift) = int.
-index(B, S) = cast_to_int(unchecked_right_shift(B, S) /\ subkey_mask).
-
-:- pragma inline(index/2).
-
-:- func mask(bitmap, shift) = bitmap.
-mask(H, S) = unchecked_left_shift(1, index(H, S)).
-
-:- pragma inline(mask/2).
-
-% This array index is computed by counting the number of 1-bits below the
-% 'index' represented by the mask.
-%
-% >>> sparseIndex 0b0110_0110 0b0010_0000
-% 2
-
-:- func sparse_index(bitmap, bitmap) = int.
-sparse_index(B, M) = weight(B /\ (M - 1) ).
-
-:- pragma inline(sparse_index/2).
-
-
-% Hamming weight, or 'popcount'
-:- func weight(uint) = int.
-
-weight(I) = 
-	(if I =< 1
-	then 
-		cast_to_int(I)
-	else 
-		weight(I /\ (I-1) ) + 1
-	).
 	
 	
 %-----------------------------------------------------------------------------%
@@ -167,6 +122,75 @@ collision(Hash, L1, L2) = collsision(Hash, Array) :-
 :- func two(shift, hash, K, V, hashmap_leaf) = ht(K, V).
 
 two(S, H1, K1, V1, L2@leaf(H2, K2, V2)) = indexed_branch(Bitmap, Array) :-
+	mask(H1, S, Bp1),
+	mask(H2, S, Bp2),
+	( if Bp1 = Bp2
+	then
+		array.init(1, two(next_shift(S), H1, K1, V1, L2), Array),
+		Bitmap = Bp1
+	else
+		array.init(2, leaf(H1, K1, V1), Array0),
+		Index = (index(Bp1, S) < index(Bp2, S) -> 1 ; 0),
+		array.set(Index, L2, Array0, Array),
+		Bitmap = Bp1 \/ Bp2		
+	).
+	
+	
+
+
+%-----------------------------------------------------------------------------%
+% Bit twiddling
+
+:- func bits_per_subkey = int.
+bits_per_subkey = 5.
+
+:- func maxchildren = int.
+maxchildren = unchecked_left_shift(1, bits_per_subkey).
+
+:- func subkey_mask = bitmap.
+subkey_mask = unchecked_right_shift(1, bits_per_subkey) - 1.
+
+:- func index(bitmap, shift) = int.
+index(B, S) = cast_to_int(unchecked_right_shift(B, S) /\ subkey_mask).
+:- pragma inline(index/2).
+
+:- func mask(hash, shift) = bitmap.
+mask(H, S) = unchecked_left_shift(1, index(H, S)).
+:- pragma inline(mask/2).
+
+:- pred mask(hash::in, shift::in, bitmap::out) is det.
+mask(H, S, mask(H, S)).
+:- pragma inline(mask/3).
+
+
+% This array index is computed by counting the number of 1-bits below the
+% 'index' represented by the mask.
+%
+% >>> sparseIndex 0b0110_0110 0b0010_0000
+% 2
+
+:- func sparse_index(bitmap, bitmap) = int.
+sparse_index(B, M) = weight(B /\ (M - 1) ).
+
+:- pragma inline(sparse_index/2).
+
+% Increment a 'Shift' for use at the next deeper level.
+func next_shift(shift) = shift.
+next_shift(S) = S + bitsPerSubkey.
+
+:- pragma inline(next_shift/1).
+
+
+% Hamming weight, or 'popcount'
+:- func weight(uint) = int.
+
+weight(I) = 
+	(if I =< 1
+	then 
+		cast_to_int(I)
+	else 
+		weight(I /\ (I-1) ) + 1
+	).
 	
 
 %-----------------------------------------------------------------------------%
