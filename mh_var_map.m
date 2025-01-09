a0ebf58312b9b5ad2ebee6b51b72bd9f6fcd6830
C4Cypher <c4cypher@gmail.com>
2025-01-09 13:16:02 -0500

    iterators and union written

--- mh_var_map.m
+++ mh_var_map.m
@@ -22,7 +22,7 @@
 
 :- import_module mh_term.
 :- import_module mh_var_id.
-:- import_module var_set.
+:- import_module mh_var_set.
 
 %-----------------------------------------------------------------------------%
 % Variable map
@@ -77,6 +77,47 @@
 :- func lookup(mh_var_map(T), mh_var) = mh_term.
 
 %-----------------------------------------------------------------------------%
+% Iterator 
+
+:- type var_map_iterator.
+
+% Retreive  first element of a var map (left traversal) and an iterator, 
+% fails if var map is empty
+:- pred first(mh_var_map(T)::in, T::out, var_map_iterator::out) 
+	is semidet.
+
+% Also produce the corresponding var_id
+:- pred first(mh_var_map(T)::in, var_id::out, T::out, var_map_iterator::out) 
+	is semidet.
+% Throws an exception if var_map is empty
+:- pred det_first(mh_var_map(T)::in, var_id::out, T::out, 
+	var_map_iterator::out) is det.
+
+:- pred det_first(mh_var_map(T)::in, T::out, var_map_iterator::out) is det.
+	
+
+
+
+% next(Map, Elem, Last, Current)
+% Retreive the next element using the iterator, fail if no more elements
+:- pred next(mh_var_map(T)::in, T::out, var_map_iterator::in, 
+	var_map:iterator::out) is semidet.
+
+:- pred next(mh_var_map(T)::in, var_id::out, T::out, var_map_iterator::in, 
+	var_map:iterator::out) is semidet.
+	
+	
+% For generating new var_map arrays
+:- pred init_first(mh_var_set::in, var_id::out, var_map_iterator::out) is det.
+
+% Fails if there should be no next element
+:- pred init_next(mh_var_set::in, var_id::out, var_map_iterator::in,
+	var_map_iterator::out) is semidet.
+	
+% Array index from iterator
+:- func iterator_index(var_map_iterator) = int.
+
+%-----------------------------------------------------------------------------%
 % Insertion
 
 	% Insert an element into a hashmap, fails if the element already exists 
@@ -127,6 +168,8 @@
 
 	% Remove a key-value pair from a map and return the value.
 	% Fail if the key is not present.
+:- pred id_remove(var_id::in, T::out, var_map(T)::in, var_map(T)::out) 
+	is semidet.
 :- pred remove(mh_var::in, T::out, var_map(T)::in, var_map(T)::out) 
 	is semidet.
 
@@ -135,14 +178,34 @@
 
 	% Delete a key-value pair from a map.
 	% If the key is not present, leave the map unchanged.	
-:- pred delete(mh_var::in, var_map(T)::in, var_map(T)::out) is det
-	<= hashable(K).
-:- func delete(var_map(T), K) = var_map(T).
+:- pred id_delete(var_id::in, var_map(T)::in, var_map(T)::out) is det.
+:- pred delete(mh_var::in, var_map(T)::in, var_map(T)::out) is det.
 
-:- func delete_list(var_map(T), list(K)) = var_map(T).
-:- pred delete_list(list(K)::in, var_map(T)::in, var_map(T)::out)
+:- pred id_delete_list(list(var_id)::in, var_map(T)::in, var_map(T)::out)
+	is det.
+:- pred delete(list(mh_var)::in, var_map(T)::in, var_map(T)::out)
 	is det.	
 	
+%-----------------------------------------------------------------------------%
+% Set operations
+
+:- func union(func(T, T) = T, var_map(T), var_map(T)) = var_map(T).
+
+:- pred union(func(T, T) = T, var_map(T), var_map(T), var_map(T)).
+:- mode union(in(func(in, in) = out is det), in, in, out) is det.
+:- mode union(in(func(in, in) = out is semidet), in, in, out) is semidet.
+
+:- func intersect(func(T, T) = T, var_map(T), var_map(T)) = 
+	var_map(T).
+	
+:- pred intersect(func(T, T) = T, var_map(T), var_map(T), var_map(T)).
+:- mode intersect(in(func(in, in) = out is det), in, in, out) is det.
+:- mode intersect(in(func(in, in) = out is semidet), in, in, out) is semidet.
+
+:- func difference(var_map(T), var_map(_)) = var_map(T).
+
+:- pred difference(var_map(T)::in, hashmap(K, _)::in, var_map(T)::out)
+	is det.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -225,6 +288,56 @@ lookup(Map, var(ID), Term) :-
 
 lookup(var_map, Var) = Term :- lookup(var_map, Var, Term).
 
+
+%-----------------------------------------------------------------------------%
+% Iterator 
+
+:- type var_map_iterator = int.
+
+first(var_map(_, A), array.lookup(A, Index), Index@0).
+first(var_map(S, A), id_reverse_sparse_index(Index, S), array.lookup(A, Index), 
+	Index@0)
+
+det_first(Map, T, I) :-
+	( if first(Map, T0, I0)
+	then 
+		T = T0,
+		I = I0
+	else
+	report_lookup_error(
+		"mh_var_map.det_first: Cannot produce iterator on empty map.")
+	).
+	
+det_first(Map, ID, T, I) :-
+	( if first(Map, ID0, T0, I0)
+	then 
+		ID = ID0,
+		T = T0,
+		I = I0
+	else
+	report_lookup_error(
+		"mh_var_map.det_first: Cannot produce iterator on empty map.")
+	).
+	
+next(var_map(_, A), array.lookup(A, Current), Last, Current) :-
+	Last < max(A),
+	Current = Last + 1.
+	
+next(var_map(S, A), id_reverse_sparse_index(Current, S),
+	array.lookup(A, Current), Last, Current) :-
+	Last < max(A),
+	Current = Last + 1.	
+	
+init_first(S, ID, Index@0) :-
+	(if id_reverse_sparse_index(Index, S) = FirstID
+	then ID = FirstID
+	else unexpected($pred, "Unable to produce ID for new var map iterator")
+	).
+
+init_next(S, id_reverse_sparse_index(Index, S), Last, Index@(Last + 1) ).
+
+iterator_index(I) = I.
+	
 %-----------------------------------------------------------------------------%
 % Insertion
 
@@ -315,3 +428,110 @@ det_update(Var, T, !Map) :-
 	else report_lookup_error(
 		"mh_var_map.det_update: Var ID not present in map", Var, !.Map)
 	).
+
+%-----------------------------------------------------------------------------%
+% Removal
+
+id_remove(ID, T, var_map(Set0, Array0), Map) :-
+	id_sparse_index(ID, Set0, Index),
+	var_set_remove_id(ID, Set0, Set),
+	lookup(Array0, Index, T),
+	(if empty_var_set(Set)
+	then
+		Map = empty_var_map
+	else
+		array_delete(Index, Array0, Array),
+		Map = var_map(Set, Array)
+	).
+	
+remove(var(ID), T, !Map) :- id_remove(ID, T, !Map).
+
+det_remove(Var, T, !Map) :-	
+	(if remove(Var, T, !Map)
+	then !:Map = !.Map
+	else report_lookup_error(
+		"mh_var_map.det_remove: Var ID not present in map", Var, !.Map)
+	).
+
+id_delete(ID, !Map) :-
+	some [!Set, !Array] (
+		!:Map = 
+			(if
+				!.Map = var_map(!.Set, !.Array),
+				id_sparse_index(ID, !.Set, Index),
+				var_set_remove_id(ID, !Set),			
+			then
+				(if 
+					not empty_var_set(!.Set),
+					array_delete(Index, !Array)	
+				then var_set(!.Set, !.Array)
+				else empty_var_map
+				)
+			else
+				!.Map
+			)
+	).
+
+delete(var(ID), T, !Map) :- id_delete(ID, T, !Map).
+
+id_delete_list([], !Map).
+id_delete_list([ID | IDs], !Map) :- 
+	id_delete(ID, !Map),
+	id_delete_list(IDs, !Map).
+	
+delete([], !Map).
+delete([ID | IDs], !Map) :- 
+	delete(ID, !Map),
+	delete(IDs, !Map).
+	
+%-----------------------------------------------------------------------------%
+% Set operations	
+
+union(F, M1, M2) = M :-
+    union(F, M1, M2, M).
+	
+union(_, empty_var_map, empty_var_map, empty_var_map).
+union(_, empty_var_map, M, M).
+union(_, M, empty_var_map, M).
+
+union(F, var_map(S1, A1), M1@var_map(S2, A2), M2@var_map(S, FinalArray)) :-
+	var_set_union(S1, S2, S),
+	var_set_count(S, ArraySize),
+	init_first(S, FirstID, Iter),
+	array.init(ArraySize, element_union(F, M1, M2, FirstID), NewArray),
+	union_loop(F, M1, M2, S, Iter, NewArray, FinalArray).
+	
+:- func element_union(func(T, T) = T, var_map(T), var_map(T), var_id) = T.
+:- mode element_union(in(func(in, in) = out is det), in, in, in) = out is det.
+:- mode element_union(in(func(in, in) = out is semidet), in, in) = out
+	is semidet.
+	
+element_union(F, M1, M2, ID) = 
+	(if id_search(M1, ID) = T1
+	then
+		(if id_search(M2, ID) = T2
+		then
+			F(T1, T2)
+		else
+			T1
+		)
+	else id_lookup(M2, ID)
+	).
+	
+:- pred union_loop(func(T, T) = T, var_map(T), var_map(T), mh_var_set, 
+	var_map_iterator, array(T), array(T)).
+:- mode union_loop(in(func(in, in) = out is det), in, in, in, in,
+	array_di, array_uo)	is det.
+:- mode union_loop(in(func(in, in) = out is semidet), in, in, in, in, 
+	array_di, array_uo) is semidet.
+	
+union_loop(F, M1, M2, S, Last, !Array) :-
+	(if init_next(S, ID, Last, Current)
+	then
+		array.set(iterator_index(Current), element_union(F, M1, M2, ID), !Array),
+		union_loop(F, M1, M2, S, Current, !Array)
+	else
+		true
+	).
+	
+	
