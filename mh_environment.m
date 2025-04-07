db13fe11480f9f1460947480ab03220e98e0bd29
C4Cypher <c4cypher@gmail.com>
2025-04-07 09:08:30 -0400

    basic binding interface, wip

--- mh_environment.m
+++ mh_environment.m
@@ -31,28 +31,64 @@
 
 :- pred is_empty(mh_environment::in) is det.
 
-:- pred ask(mh_environment::in, mh_term::in, mh_term::out) is det.
+%-----------------------------------------------------------------------------%
+% Queries
 
-:- func ask(mh_envirronment, mh_term) = mh_term.
+% E.T := B  TODO: Find a binding operator for the query
+% check to see if two terms are bound in the given environment mapping
+% This is a partial check that only involves the scope of the environment
+% itself. Is not reflexive, will only succeed if the first term is bound
+% to the second. 
 
-% Needs event logging output, need to define events
-:- pred assert(mh_environment::in, mh_term::in, mh_environment::out) is det.
+:- pred bound(mh_environment, mh_term, mh_term).
+:- mode bound(in, in, in) is semidet.
+:- mode bound(in, in, out) is semidet. 
+:- mode bound(in, out, out) is nondet.
 
-:- func assert(mh_environment, mh_term) = mh_environment.
+% E.T := _
+% Succeeds if a term is bound, without retreiving the bound term
+:- pred bound(mh_environment, mh_term).
+:- mode bound(in, in) is semidet.
+:- mode bound(in, out) is nondet.
+
+
+%:- pred ask(mh_environment::in, mh_term::in, mh_term::out) is det.
+
+%:- func ask(mh_envirronment, mh_term) = mh_term.
+
+
+%-----------------------------------------------------------------------------%
+% Changes
+
+
+% E.T = B  TODO: Find a better operator for this, semantically the unification
+% operator works, but this is a very specific context
+% bind two terms, effectively unifying them. This overwrites any existing
+% binding for the term. 
+:- pred bind(mh_term::in, mh_term::in, 
+	mh_environment::in, mh_environment::out) is det.
+	
+% remove an existing binding for a term, if any.
+:- pred unbind(mh_term::in, mh_environment::in, mh_environment::out) is det.
+
+%:- pred assert(mh_environment::in, mh_term::in, mh_environment::out) is det.
+
+%:- func assert(mh_environment, mh_term) = mh_environment.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
 
-:- import_module mh_environment.map.
+:- import_module mh_substitution.
 
 %-----------------------------------------------------------------------------%
 
 :- type mh_environment
-	--->	map_env(mh_term_map(mh_term)) % Unification map
-	;		term_env(mh_term) % The context and scope of a given term.
-	;		module_env(
-		interface::mh_environment, %Replace with declarations
-		implementation::mh_environment
-		).
+	--->	map_env(mh_term_map(mh_term)). % Unification map
+%	;		term_env(mh_term) % The context and scope of a given term.
+%	;		sub_env(mh_substitution) % only a set of variable bindings
+%	;		module_env(
+%		interface::mh_environment, %Replace with declarations
+%		implementation::mh_environment
+%		).
