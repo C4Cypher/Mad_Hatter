4fa1f48cc7b97eb412c19db6a8ca2eeccb8887f6
C4Cypher <c4cypher@gmail.com>
2025-04-14 10:24:38 -0400

    substitution refactor complete

--- mh_substitution.m
+++ mh_substitution.m
@@ -30,7 +30,8 @@
 	--->	sub_map(mh_var_map(mh_term))
 	;		ren_map(mh_var_map(var_id)).
 	
-
+:- inst term_sub
+	--->	sub_map(ground).
 	
 :- pred init_sub(mh_substitution::out) is det.
 :- func init_sub = mh_substitution.
@@ -47,8 +48,21 @@
 :- pred sub_from_offset_array(array(mh_term)::in, var_id_offset::in, 
 	mh_substitution::out) is det.
 	
-:- func sub_from_offset_array(array(mh_term), var_id_offset) = mh_substitution.
+:- func sub_from_offset_array(array(mh_term), var_id_offset) = mh_substitution
 
+
+% If a substitution consists only of var mappings, convert the internal
+% representation to a renaming
+:- pred sub_to_ren(mh_substitution::in, mh_substitution::out(mh_renaming))
+	is semidet.
+
+:- func sub_to_ren(mh_substitution::in) = 
+	(mh_substitution::out(mh_renaming)) is semidet.
+	
+% If a substitution is in renaming form, convert to a var mapping substitution
+:- pred ren_to_sub(mh_substitution::in, mh_substitution::out(term_sub)) is det.
+:- func ren_to_sub(mh_substitution::in) = (mh_substitution::out(term_sub))
+	is det.
 
 %-----------------------------------------------------------------------------%
 % Bounds
@@ -57,7 +71,7 @@
 	var_id_set::out) is semidet.
 	
 	
-:- pred substitution_bounds_det(mh_substitution::in, var_id_offset::out, 
+:- pred det_substitution_bounds(mh_substitution::in, var_id_offset::out, 
 	var_id_set::out) is det.
 
 	
@@ -115,9 +129,12 @@
 :- type mh_renaming =< mh_substitution
 	---> 	ren_map(mh_var_map(var_id)).
 	
+	
 :- mode is_renaming == ground >> mh_renaming.
 
 :- pred is_renaming(mh_substitution::is_renaming) is semidet.
+
+
 
 :- pred init_ren(mh_renaming::out) is det.
 :- func init_ren = mh_renaming.
@@ -135,10 +152,6 @@
 	mh_renaming::out) is det.
 	
 :- func ren_from_offset_array(array(var_id), var_id_offset) = mh_renaming.
-
-:- pred sub_to_renaming(mh_substitution::in, mh_renaming::out) is semidet.
-
-:- func sub_to_renaming(mh_substitution) = mh_renaming is semidet.
 
 
 %-----------------------------------------------------------------------------%
@@ -186,6 +199,7 @@
 
 :- func compose_renamings(mh_renaming, mh_renaming) = 
 	mh_renaming.
+	
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -195,6 +209,7 @@
 :- import_module int.
 :- import_module require.
 :- import_module string.
+:- import_module pair.
 
 %-----------------------------------------------------------------------------%
 % 	Substitutions
@@ -216,8 +231,20 @@ sub_from_offset_array(Array, Offset, sub_from_offset_array(Array, Offset)).
 
 sub_from_offset_array(Array, Offset) = 
 	sub_map(mh_var_map.from_offset_array(Array, Offset)).
+	
+sub_to_ren(sub_map(!.Map), ren_map(!:Map)) :- map_id(var_to_id, !Map).
+sub_to_ren(Sub, Sub) :- is_renaming(Sub).
 
+:- pred var_to_id(_::unused, mh_term::in, var_id::out) is semidet.
 
+var_to_id(_, var(ID), ID).
+
+ren_to_sub(ren_map(!.Map), sub_map(!:Map)) :- map_id(id_to_var, !Map).
+ren_to_sub(Sub@sub_map(_), Sub).
+
+:- pred id_to_var(_::unused, var_id::in, mh_term::out) is det.
+
+id_to_var(_, ID, var(ID)).
 
 %-----------------------------------------------------------------------------%
 % Bounds
@@ -232,26 +259,21 @@ substitution_bounds(ren_array(Map),	Offset, Set) :-
 	else var_map_bounds(Map, Offset, Set)
 	).
 	
-substitution_bounds_det(sub_array(Map),	Offset, Set) :- 
+det_substitution_bounds(sub_array(Map),	Offset, Set) :- 
 	var_map_bounds(Map, Offset, Set)).
 	
-substitution_bounds_det(ren_array(Map),	Offset, Set) :- 
+det_substitution_bounds(ren_array(Map),	Offset, Set) :- 
 	var_map_bounds(Map, Offset, Set)).
-	
 
 %-----------------------------------------------------------------------------%
 % Looking up variables in substitutions
 
-sub_contains_id(sub_unbound, _) :- fail.
 sub_contains_id(sub_array(Map), ID) :- contains_id(Map, ID).
 sub_contains_id(ren_array(Map), ID) :- contains_id(Map, ID).
 
-substitution_offset(ren_offset(Offset, _), Offset).
-substitution_offset(ren_offset(Offset, _)) = Offset.
-
+sub_id_search(Sub, ID, sub_id_search(Sub, ID)).
 sub_id_search(sub_map(Map), ID) = id_search(Map, ID).
 sub_id_search(ren_map(Map), ID) = var(id_search(Map, ID)).
-
 
 sub_id_lookup(Sub, ID, Term) :-
 	( if sub_id_search(Sub, ID, Found)
@@ -265,226 +287,56 @@ sub_var_search(Sub, Var) = Term :- sub_var_search(Sub, Var, Term).
 
 sub_var_lookup(Sub, var(ID), Term) :- sub_id_lookup(Sub, ID, Term).
 sub_var_lookup(Sub, Var) = Term :- sub_var_lookup(Sub, Var, Term).
-
 	
 %-----------------------------------------------------------------------------%
 % Substitution composition
 
 compose_substitutions(Sub1, Sub2, Sub3) :-
-promise_equivalent_solutions [Sub3] 
-	(
-		empty_substitution(Sub1), Sub3 = Sub2
-	;	
-		empty_substitution(Sub2), Sub3 = Sub1
-	;
-		Sub1 = sub_map(Map1), Sub2 = sub_map(Map2),
-		first(Map2, First, Iterator),
-		compose_sub_sub
-	
-)
-
-:- pred compose_sub_sub(mh_var_map(mh_term)::in, mh_var_map(mh_term)::in,
-	var_map_iterator::in, mh_substitution::out) is det.
-
-/* Depriciated
-compose_substitutions(Sub1, Sub2, Sub) :-
-	( if promise_equivalent_solutions [Sub3]
-		compose_sub_special(Sub1, Sub2, Sub3)
+	if promise_equivalent_solutions [SubTemp] 
+		(
+			empty_substitution(Sub1), SubTemp = Sub2
+		;	
+			empty_substitution(Sub2), SubTemp = Sub1
+		)
 	then
-		Sub = Sub3
+		Sub3 = SubTemp
+	else if
+		Sub1 = ren_map(Map1), Sub2 = ren_map(Sub2)
+	then
+		fold_id(remap_renaming(Map2), Map1, Map1 - Map2, Map03 - MapDiff),
+		fold_id(mh_var_map.det_insert, MapDiff, Map03, Map3),
+		Sub3 = ren_map(Map3)
 	else
-		substitution_bounds_det(Sub1, Offset1, Set1),
-		substitution_bounds_det(Sub2, Offset2, Set2),
-		(offset_lt(Offset1, Offset2) -> Offset = Offset1 ; Offset = Offset2 ),
-		(var_id_set_lt(Set1, Set2) -> Set = Set1 ; Set = Set2),
-		var_id_set_init_array(Offset, Set, nil, Array0),
-		compose_substiution_step(
-			first_var_id(Offset), last_var_id(Set),
-			Sub1, Sub2,
-			Offset, Array0, Array1
-		),
-		init_array_sub(Array1, Offset, Sub)
-	).
+		ren_to_sub(Sub1) = sub_map(Map1), ren_to_sub(Sub2) = sub_map(Map2),
+		fold_id(remap_substitution(Sub2), Map1, Map1 - Map2, Map03 - MapDiff),
+		fold_id(mh_var_map.det_insert, MapDiff, Map03, Map3),
+		Sub3 = ren_map(Map3).
+
+:- pred remap_substitution(mh_var_map(mh_term)::in, var_id::in, mh_term::in, 
+	pair(mh_var_map(mh_term), mh_var_map(mh_term))::in, 
+	pair(mh_var_map(mh_term), mh_var_map(mh_term))::out) is det.
 	
-compose_substitutions(Sub1, Sub2) = Sub :- 
-	compose_substitutions(Sub1, Sub2, Sub).
-		
-		
-:- pred compose_sub_special(mh_substitution::in, mh_substitution::in,
-	mh_substitution::out) is cc_nondet.
-
-
-compose_sub_special(S, sub_empty, S).
-compose_sub_special(sub_empty, S, S).
-
-compose_sub_special(S, ren_empty, S).
-compose_sub_special(ren_empty, S, S).
-
-
-
-compose_sub_special(
-	sub_single(ID1, Term1), 
-	sub_single(ID2, Term2),
-	Sub
-) :-
-	( if Term1 = var(ID2)
+remap_substitution(Map2, From1, To1, !.Map3 - !.MapDiff, !:Map3 - !:MapDiff) 
+:-
+	(if 
+		To1 = var(Var1),
+		id_search(Map2, Var1, To2)
 	then
-		Sub = sub_single(ID1, Term2)
-	else 
-		compare(Comp, ID1, ID2),
-		(
-			Comp = (=),
-			Sub = sub_single(ID1, Term1)
-		;
-			(
-				Comp = (>),
-				first_var_id(Offset) = ID2,
-				last_var_id(Set) = ID1
-			;
-				Comp = (<),
-				first_var_id(Offset) = ID1,
-				last_var_id(Set) = ID2
-			),
-			var_id_set_init_array(Offset, Set, nil, A0),
-			var_id_set(ID1, Term1, Offset, A0, A1),
-			var_id_set(ID2, Term2, Offset, A1, A2),
-			init_array_sub(A2, Offset, Sub)
-		)
-		
-	).
-	
-	
-compose_sub_special(
-	ren_single(ID1, Var1),
-	sub_single(ID2, Term2),
-	Sub
-) :- 
-	( if ID2 = Var1
-	then
-		Sub = sub_single(ID1, Term2)
-	else 
-		compare(Comp, ID1, ID2),
-		(
-			Comp = (=),
-			Sub = ren_single(ID1, Var1)
-		;
-			(
-				Comp = (>),
-				first_var_id(Offset) = ID2,
-				last_var_id(Set) = ID1
-			;
-				Comp = (<),
-				first_var_id(Offset) = ID1,
-				last_var_id(Set) = ID2
-			),
-			var_id_set_init_array(Offset, Set, nil, A0),
-			var_id_set(ID1, var(Var1), Offset, A0, A1),
-			var_id_set(ID2, Term2, Offset, A1, A2),
-			init_array_sub(A2, Offset, Sub)
-		)
-		
-	).
-
-compose_sub_special(
-	sub_single(ID1, Term1),
-	ren_single(ID2, Var2),
-	Sub
-) :-
-	( if Term1 = var(ID2)
-	then
-		Sub = ren_single(ID1, Var2)
-	else 
-		compare(Comp, ID1, ID2),
-		promise_equivalent_solutions [Sub]
-		(
-			Comp = (=),
-			Sub = sub_single(ID1, Term1)
-		;
-			(
-				Comp = (>),
-				first_var_id(Offset) = ID2,
-				last_var_id(Set) = ID1
-			;
-				Comp = (<),
-				first_var_id(Offset) = ID1,
-				last_var_id(Set) = ID2
-			),
-			var_id_set_init_array(Offset, Set, nil, A0),
-			var_id_set(ID1, Term1, Offset, A0, A1),
-			var_id_set(ID2, var(Var2), Offset, A1, A2),
-			init_array_sub(A2, Offset, Sub)
-		)
-		
-	).
-
-	
-:- pred compose_substiution_step(
-	var_id, var_id,
-	mh_substitution, mh_substitution,
-	var_id_offset, array(mh_term), array(mh_term)
-).
-
-:- mode compose_substiution_step(
-	in, in, 
-	in, in, 
-	in, array_di, array_uo) is det.
-	
-compose_substiution_step(
-	Current, Last,
-	Sub1, Sub2,
-	Offset, !Array
-) :-
-	% Look up the current id in the first sub, if the found term is a variable
-	% pass it through the second sub, if it is not a variable, 
-	(if sub_id_search(Sub1, Current, Term1)
-	then
-		(if	Term1 = var(Term1_ID)
-		then
-			(if sub_id_search(Sub2, Term1_ID, Term2)
-			then
-				var_id_set(Current, Term2, Offset, !Array)
-			else
-				var_id_set(Current, Term1, Offset, !Array)
-			)
-		else 
-			var_id_set(Current, Term1, Offset, !Array)
-		)
-	else
-		(if sub_id_search(Sub2, Current, Term)
-		then
-			var_id_set(Current, Term, Offset, !Array)
-		else
-			!:Array = !.Array
-		)
+		id_update(From1, To2, !Map3)
+	else true	
 	),
+	id_delete(From1, !MapDiff).
 	
-	(if var_id_ge(Current, Last)
-	then true
-	else
-		compose_substiution_step(
-			next_var_id(Current), Last, 
-			Sub1, Sub2, 
-			Offset, !Array)
-	).
 
-*/ 	
-	
 %-----------------------------------------------------------------------------%
 % Renaming
 
-is_renaming(ren_empty). 
-is_renaming(ren_single(_, _) ).
-is_renaming(ren_array(_) ).
-is_renaming(ren_array(_, _) ).
-is_renaming(ren_offset(_, _)).
+is_renaming(ren_map(_)).
 
 init_ren(init_ren).
-init_ren = ren_empty.
+init_ren = ren_map(init).
 
-empty_renaming(ren_empty).
-empty_renaming(ren_array(make_empty_array)).
-empty_renaming(ren_array(make_empty_array, _)).
-empty_renaming(ren_offset(null_var_id_offset, _)).
+empty_renaming(ren_map(init)).
 
 %-----------------------------------------------------------------------------%
 % Conversion
@@ -501,104 +353,71 @@ ren_from_offset_array(Array, Offset) =
 %-----------------------------------------------------------------------------%
 % Bounds
 
-
 renaming_bounds(ren_array(Map),	Offset, Set) :- 
 	(if empty_var_map(Map) then fail
 	else var_map_bounds(Map, Offset, Set)
 	).
 
-	
 renaming_bounds_det(ren_array(Map),	Offset, Set) :- 
 	var_map_bounds(Map, Offset, Set)).
 	
 %-----------------------------------------------------------------------------%
 % Looking up variables in renamings
 
-ren_contains_id(ren_empty, _) :- fail.
-ren_contains_id(ren_single(ID, _), ID).
+ren_contains_id(ren_array(Map), ID) :- contains_id(Map, ID).
 
-ren_contains_id(ren_array(Array), ID1) :- 
-	var_id_semidet_lookup(Array, ID1, ID2),
-	var_id_gt(ID2, null_var_id).
-
-ren_contains_id(ren_array(Array, Offset), ID1) :- 
-	var_id_semidet_lookup(Array, Offset, ID1, ID2),
-	var_id_gt(ID2, null_var_id).
-	
-ren_contains_id(ren_offset(Offset, Set), ID) :- 
-	contains_var_id(Offset, Set, ID).
-
-
-renaming_offset(ren_offset(Offset, _), Offset).
-
-%-----------------------------------------------------------------------------%
-
-
-ren_id_search(ren_empty, _, null_var_id) :- fail.
-
-ren_id_search(ren_single(ID1, ID2), ID1, ID2).
-	
-ren_id_search(ren_array(Array), ID1, ID2) :- 
-	var_id_semidet_lookup(Array, ID1, ID2),
-	var_id_gt(ID2, null_var_id).
-	
-
-ren_id_search(ren_array(Array, Offset), ID1, ID2) :-
-	var_id_semidet_lookup(Array, Offset, ID1, ID2),
-	var_id_gt(ID2, null_var_id).
-	
-ren_id_search(ren_offset(Offset, Set), !ID) :- 
-	contains_var_id(Set, !.ID),
-	var_id_offset(!ID, Offset).
-
-ren_id_search(Ren, !.ID) = !:ID :- ren_id_search(Ren, !ID).
-
-%-----------------------------------------------------------------------------%
+ren_id_search(Ren, ID, ren_id_search(Ren, ID)).
+ren_id_search(ren_map(Map), ID) = var(id_search(Map, ID)).
 
 ren_id_lookup(Ren, !ID) :-
-	( if ren_id_search(Ren, !.ID, Found)
+	if ren_id_search(Ren, !.ID, Found)
 	then !:ID = Found
-	else !:ID = !.ID).
+	else !:ID = !.ID.
 	
 ren_id_lookup(Ren, !.ID) = !:ID :- ren_id_lookup(Ren, !ID).
 
-%-----------------------------------------------------------------------------%
-
 ren_var_search(Ren, var(ID1), ID2) :- ren_id_search(Ren, ID1, ID2).
-
 ren_var_search(Ren, Var) = ID :- ren_var_search(Ren, Var, ID).
 
-%-----------------------------------------------------------------------------%
-
 ren_var_lookup(Ren, var(!.ID), var(!:ID)) :- ren_id_lookup(Ren, !ID).
-
 ren_var_lookup(Ren, !.Var) = !:Var :- ren_var_lookup(Ren, !Var).
 
 %-----------------------------------------------------------------------------%
 % Renaming composition
 
 compose_renamings(Ren1, Ren2, Ren3) :-
-promise_equivalent_solutions [Ren3] 
+	if promise_equivalent_solutions [Ren3] 
 	(
-		empty_renaming(Ren1), Ren3 = Ren2
+		empty_renaming(Ren1), RenTemp = Ren2
 	;	
-		empty_renaming(Ren2), Ren3 = Ren1
-	;
-		Ren1 = Ren_map(Map1), Ren2 = Ren_map(Map2),
+		empty_renaming(Ren2), RenTemp = Ren1
+	)
+	then
+		Ren3 = RenTemp
+	else
+		Ren1 = ren_map(Map1), Ren2 = ren_map(Map2),
+		fold_id(remap_renaming(Map2), Map1, Map1 - Map2, Map03 - MapDiff),
+		fold_id(mh_var_map.det_insert, MapDiff, Map03, Map3),
+		Ren3 = ren_map(Map3).
+
+
+
+:- pred remap_renaming(mh_var_map(var_id)::in, var_id::in, var_id::in, 
+	pair(mh_var_map(var_id), mh_var_map(var_id))::in, 
+	pair(mh_var_map(var_id), mh_var_map(var_id))::out) is det.
 	
-)
+remap_renaming(Map2, From1, To1, !.Map3 - !.MapDiff, !:Map3 - !:MapDiff) 
+:-
+	(if id_search(Map2, To1, To2)
+	then
+		id_update(From1, To2, !Map3)
+	else true	
+	),
+	id_delete(From1, !MapDiff).
 
-:- pred compose((func(T) = var_id), mh_var_map(T), var_id, T, var_map_iterator, 
-	mh_var_map(T), mh_var_map(T)).
-:- mode compose(in(func(in) = out is semidet), in, in, in, in, in, out) is det.
-:- mode compose(in(func(in) = out is det), in, in, in, in, in, out) is det.
 
-compose(ToVar, Map2, ID, T,  Iter, !Map) :-
-	(if )
 
-:- func identity(X) = X.
-
-identity(X) = X.
+compose_renamings(Ren1, Ren2) = Ren3 :- compose_renamings(Ren1, Ren2, Ren3).
 
 %-----------------------------------------------------------------------------%
 % Utility
