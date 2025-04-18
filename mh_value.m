0f1d4b5aa4d0546becf1220de2df89631a3a5c4b
C4Cypher <c4cypher@gmail.com>
2025-04-18 11:48:41 -0400

    refine values

--- mh_value.m
+++ mh_value.m
@@ -16,20 +16,41 @@
 :- interface.
 
 :- import_module bool.
+:- import_module univ.
+
+:- import_module mh_symbol.
 
 %-----------------------------------------------------------------------------%
 
-:- type mh_value
+:- type mh_value  % struct union  struct { bool foreign, union {}}?
 	--->	mh_value(
-		c_pointer::word, 
-		char::size, 
-		bool::literal, 
-		mh_value_format::format
-	).
+				word::c_pointer, 
+				size::uint, 	% width of the value in bytes
+				literal::bool,	% word/1 is a pointer if literal is false 
+				format::mh_value_format	
+			)
+			
+	;		foreign_value(
+				foreign_word::c_pointer,
+				language::mh_symbol, 
+				value_type::mh_symbol
+			)
+	;		mr_value(univ).
 	
+	/* bytecode constructors
+	
+	;		bytecode_chunk(start::c_pointer, chunk_size::uint)
+	;		bytecode_term(term_pointer::c_pointer, parent_chunk::mh_chunk).
+	
+:- type mh_chunk =< mh_value
+	--->	bytecode_chunk(start::c_pointer, chunk_size::uint).
+	*/
+			
+% Todo:   See if I can organize a c union of structs that fits nicely together
+% 			for these constructors
+
 :- type mh_value_format
-	--->	void	% foreign void *
-	;		term	% bytecode term
+	--->	void	% void *
 	;		signed_int
 	;		unsigned_int
 	;		float
