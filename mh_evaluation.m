ee0470b41492dd5ee2c557d0b118d7ff8c714e90
C4Cypher <c4cypher@gmail.com>
2025-09-15 12:30:32 -0400

    Splitting off evaluation code into it's own module
    mh_term, mh_relation and mh_proposition are crowded enough as it is simply defining the form of those types. While it would be more 'pure' to define their evaluation in their seperate modules, I'm going to break off that into it's own module, given that I want that code together in one place.

---
+++ mh_evaluation.m
@@ -0,0 +1,24 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2025 Charlie H. McGee IV.
+% This file may only be copied under the terms of the GNU Library General
+% Public License as described in the file LICENCE.
+%-----------------------------------------------------------------------------%
+% 
+% File: mh_environment.m
+% Main author: C4Cypher.
+% Stability: low.
+%-----------------------------------------------------------------------------%
+
+:- module mh_environment.
+
+:- interface.
+
+:- import_module ordered_set.
+
+:- import_module mh_term.
+:- import_module mh_term_map.
+:- import_module mh_scope.
+:- import_module mh_event.
+
