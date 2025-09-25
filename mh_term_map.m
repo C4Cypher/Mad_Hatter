e8e397acfd213720fed23c1bd46d14f8116ad948
C4Cypher <c4cypher@gmail.com>
2025-09-25 10:23:44 -0400

    insert and det_insert implemented

--- mh_term_map.m
+++ mh_term_map.m
@@ -53,8 +53,6 @@
 	% Succeeds if the map contains the given key
 :- pred contains(mh_term_map(_)::in, mh_term::in) is semidet.
 
-/* Implementation delayed until mh_term is stable
-
 	% Fails if the key is not found
 :- pred search(mh_term_map(T)::in, mh_term::in, 
 T::out) is semidet.
@@ -71,7 +69,7 @@ T::out) is semidet.
 	mh_term_map(T)::out) is semidet.
 	
 :- pred insert(mh_term::in, mh_term_set::in, mh_term_set::out) 
-	is semidet.
+	is semidet. %!
 	
 :- pred det_insert(mh_term::in, T::in, mh_term_map(T)::in, 
 	mh_term_map(T)::out) is det.
@@ -159,26 +157,19 @@ T::out) is semidet.
 :- pred difference(mh_term_map(T)::in, mh_term_map(_)::in, 
 	mh_term_map(T)::out) is det.
 
-*/
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
 
-:- import_module maybe.
-:- import_module array.
-:- import_module hash_table.
-:- import_module map.
-:- import_module type_desc.
-:- import_module univ.
+:- import_module require.
 
-:- import_module mh_term.
-:- import_module mh_symbol.
-:- import_module mh_value_map.
+:- import_module mh_symbol_map.
 :- import_module mh_var_map.
-:- import_module hashmap.
+:- import_module mh_value_map.
 :- import_module mh_tuple_map.
+:- import_module mh_relation_map.
 
 %-----------------------------------------------------------------------------%
 % Term maps
@@ -187,14 +178,13 @@ T::out) is semidet.
 :- type mh_term_map(T)
 	--->	empty_term_map
 	;		term_map(
-				atom :: symbol_map(T),
+				atom :: mh_symbol_map(T),
 				var :: mh_var_map(T),
 				value :: mh_value_map(T),
 				cons :: mh_tuple_map(T), 
 				relation :: mh_relation_map(T)
 			).
 
-:- type symbol_map(T) == hashmap(mh_symbol, T).
 
 :- func prototype_map = mh_term_map(_).
 
@@ -246,6 +236,110 @@ contains(term_map(_, _, Map, _, _), value(mr_value(Univ)) ) :-
 contains(term_map(_, _, _, Map, _), cons(Functor, Args)) :- 
 	contains(Map, tuple_cons(Functor, Args)).
 	
-contains(term_map(_, _, _, _, Map), relation(Rel)) :- contains(Map, Rel).
+contains(term_map(_, _, _, _, Map), relation(Rel)) :- search(Map, Rel).
 
+search(term_map(Map, _, _, _, _), atom(Symbol), T) :- search(Map, Symbol, T).
 
+search(term_map(_, Map, _, _, _), var(ID), T) :- id_search(Map, ID, T).
+
+search(term_map(_, _, Map, _, _), value(mr_value(Univ)), T) :- 
+	search_univ(Map, Univ, T).
+	
+search(term_map(_, _, _, Map, _), cons(Functor, Args), T) :- 
+	search(Map, tuple_cons(Functor, Args), T).
+	
+search(term_map(_, _, _, _, Map), relation(Rel), T) :- search(Map, Rel, T).
+
+search(Map, Term) = T :- search(Map, Term, T).
+
+lookup(Map, Term, T) :-
+    ( if search(Map, Term, T0) then
+        T = T0
+    else
+        report_lookup_error("mh_term_map.lookup: key not found", Term, T)
+    ).
+	
+lookup(Map, Term) = T :- lookup(Map, Term, T).
+
+%-----------------------------------------------------------------------------%
+% Insertion
+
+/*
+:- pred empty_to_prototype(mh_term_map(T)::in, mh_term_map(T)::out) is det.
+
+empty_to_prototype(!Map) :-
+	( if !.Map = empty_map then
+		!:Map = prototype_map
+	else true ).
+*/	
+	
+:- pred deconstruct_term_map(mh_term_map(T)::in, mh_symbol_map(T)::out,  
+	mh_var_map(T)::out, mh_tuple_map(T)::out, mh_relation_map::out) is det.
+
+deconstruct_term_map(empty_map, mh_symbol_map.init, mh_var_map.init,
+	mh_value_map.init, mh_tuple_map.init, mh_relation_map.init).
+
+deconstruct_term_map(term_map(Atoms, Vars, Vals, Cons, Rels), Atoms, Vars, 
+	Vals, Cons,	Rels).
+
+insert(atom(Symbol), T, !Map) :-
+	deconstruct_term_map(!.Map, Atoms0, Vars, Vals, Cons, Rels),
+	mh_symbol_map.insert(Symbol, T, Atoms0, Atoms),
+	!:Map = term_map(Atoms, Vars, Vals, Cons, Rels).
+	
+insert(var(ID), T, !Map) :-
+	deconstruct_term_map(!.Map, Atoms, Vars0, Vals, Cons, Rels),
+	mh_var_map.id_insert(ID, T, Vars0, Vars),
+	!:Map = term_map(Atoms, Vars, Vals, Cons, Rels).
+
+insert(mh_value(mr_value(Univ)), T, !Map) :-
+	deconstruct_term_map(!.Map, Atoms, Vars, Vals0, Cons, Rels),
+	mh_value_map.insert_univ(Univ, T, Vals0, Vals),
+	!:Map = term_map(Atoms, Vars, Vals, Cons, Rels).
+
+insert(cons(Functor, Args), T, !Map) :-
+	deconstruct_term_map(!.Map, Atoms, Vars, Vals, Cons0, Rels),
+	mh_symbol_map.insert(tuple_cons(Functor, Args), T, Cons0, Cons),
+	!:Map = term_map(Atoms, Vars, Vals, Cons, Rels).
+	
+insert(relation(Rel), T, !Map) :-
+	deconstruct_term_map(!.Map, Atoms, Vars, Vals, Cons, Rels0),
+	mh_relation_map.insert(Symbol, T, Rels0, Rels),
+	!:Map = term_map(Atoms, Vars, Vals, Cons, Rels).
+	
+insert(Term, !Map) :- insert(Term, unit, !Map).
+	
+det_insert(Term, T, !M) :-
+	( if insert(Term, T, !.M, NewMap) then
+        !:M = NewMap
+    else
+        report_lookup_error("mh_term_map.det_insert: term already present", 
+		Term, T)
+    ).
+	
+det_insert(Term, !Map) :-
+	(if insert(Term, !Map)
+	then !:Map = !.Map
+	else report_lookup_error(
+		"mh_term_map.det_insert: term already present", 
+		Term, !.Map)
+	).	
+	
+det_insert_from_corresponding_lists([], [], !Map).
+det_insert_from_corresponding_lists([], [_ | _], _, _) :-
+    unexpected($pred, "list length mismatch").
+det_insert_from_corresponding_lists([_ | _], [], _, _) :-
+    unexpected($pred, "list length mismatch").
+det_insert_from_corresponding_lists([K | Ks], [V | Vs], !Map) :-
+    det_insert(K, V, !Map),
+    det_insert_from_corresponding_lists(Ks, Vs, !Map).
+	
+det_insert_from_assoc_list([], !Map).
+det_insert_from_assoc_list([K - V | KVs], !Map) :-
+    det_insert(K, V, !Map),
+    det_insert_from_assoc_list(KVs, !Map).
+	
+det_insert_from_list([], !Set).
+det_insert_from_list([ Tuple | List]) :-
+	det_insert(Tuple, !Set),
+	det_insert_from_list(List, !Set).
