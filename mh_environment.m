4d23ec3b7b76260fdec270d056395d5d857916d0
C4Cypher <c4cypher@gmail.com>
2025-12-24 10:44:04 -0500

    Added environment variable calls

--- mh_environment.m
+++ mh_environment.m
@@ -15,6 +15,8 @@
 
 :- interface.
 
+:- import_module string.
+
 :- import_module mh_term.
 :- import_module mh_term_map.
 
@@ -30,6 +32,106 @@
 :- type mh_environment == mh_term_map(mh_term).
 
 %-----------------------------------------------------------------------------%
+% Environment variables
+
+% Environment variables map to atoms with symbols prefixed with the string
+% "__". The following calls prepend that prefix. The calls with an extra
+% key field are intended for 'module local' variables, appending the arguments
+% seperated by an additional "_"
+
+:- pred contains_env(mh_environment::in, string::in) is semidet.
+:- pred contains_env(mh_environment::in, string::in, string::in) is semidet.
+
+:- pred search_env(mh_environment::in, string::in, mh_term::out) is semidet.
+:- pred search_env(mh_environment::in, string::in, string::in, mh_term::out)
+	is semidet.
+	
+:- pred lookup_env(mh_environment::in, string::in, mh_term::out) is det.
+:- pred lookup_env(mh_environment::in, string::in, string::in, mh_term::out)
+	is det.
+	
+:- pred insert_env(string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is semidet.
+:- pred insert_env(string::in, string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is semidet.
+	
+:- pred det_insert_env(string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+:- pred det_insert_env(string::in, string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+	
+:- pred set_env(string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+:- pred set_env(string::in, string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+	
+:- pred update_env(string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is semidet.
+:- pred update_env(string::in, string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is semidet.
+	
+:- pred det_update_env(string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+:- pred det_update_env(string::in, string::in, mh_term::in, mh_environment::in,
+	mh_environment::out) is det.
+	
+:- pred remove_env(string::in, mh_term::out, mh_environment::in,
+	mh_environment::out) is semidet.
+:- pred remove_env(string::in, string::in, mh_term::out, mh_environment::in,
+	mh_environment::out) is semidet.
+	
+:- pred delete_env(string::in, mh_environment::in,	mh_environment::out)
+	is det.
+:- pred delete_env(string::in, string::in, mh_environment::in,
+	mh_environment::out) is det.	
+
+%-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
+
+:- import_module mh_symbol.
+
+%-----------------------------------------------------------------------------%
+% Environment variables
+
+:- func prefix(string) = mh_term.
+prefix(Name) = atom(symbol("__" ++ Name)).
+
+:- func prefix(string, string) = mh_term.
+prefix(Module, Name) = prefix(local(Module, Name)).
+
+:- func local(string, string) = string.
+local(Module, Name) = Module ++ "__" ++ Name.
+
+contains_env(Env, Name) :- contains(Env, prefix(Name)).
+contains_env(Env, Mod, Name) :- contains_env(Env, local(Mod, Name)).
+
+search_env(Env, Name, Term) :- search(Env, prefix(Name), Term).
+search_env(Env, Mod, Name, Term) :- search_env(Env, local(Mod, Name), Term).
+
+lookup_env(Env, Name, Term) :- lookup(Env, prefix(Name), Term).
+lookup_env(Env, Mod, Name, Term) :- lookup_env(Env, local(Mod, Name), Term).
+
+insert_env(Name, Term, !Env) :- insert(prefix(Name), Term, !Env).
+insert_env(Mod, Name, Term, !Env) :- insert_env(local(Mod, Name), Term, !Env).
+
+det_insert_env(Name, Term, !Env) :- det_insert(prefix(Name), Term, !Env).
+det_insert_env(Mod, Name, Term, !Env) :- 
+	det_insert_env(local(Mod, Name), Term, !Env).
+	
+set_env(Name, Term, !Env) :- set(prefix(Name), Term, !Env).
+set_env(Mod, Name, Term, !Env) :- set_env(local(Mod, Name), Term, !Env).
+
+update_env(Name, Term, !Env) :- update(prefix(Name), Term, !Env).
+update_env(Mod, Name, Term, !Env) :- update_env(local(Mod, Name), Term, !Env).
+
+det_update_env(Name, Term, !Env) :- det_update(prefix(Name), Term, !Env).
+det_update_env(Mod, Name, Term, !Env) :- 
+	det_update_env(local(Mod, Name), Term, !Env).
+	
+remove_env(Name, Term, !Env) :- remove(prefix(Name), Term, !Env).
+remove_env(Mod, Name, Term, !Env) :- remove_env(local(Mod, Name), Term, !Env).
+
+delete_env(Name, !Env) :- delete(prefix(Name), !Env).
+delete_env(Mod, Name, !Env) :- delete_env(local(Mod, Name), !Env).
