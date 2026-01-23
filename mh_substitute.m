e4dd9958f0ccea28db126aa91c1ae646176d0217
C4Cypher <c4cypher@gmail.com>
2026-01-23 11:27:07 -0500

    implemented term level substitution

--- mh_substitute.m
+++ mh_substitute.m
@@ -16,10 +16,14 @@
 :- interface.
 
 :- import_module mh_evaluation.
-:- import_module mh_term.
-:- import_module mh_substitution.
 :- import_module mh_environment.
 :- import_module mh_scope.
+:- import_module mh_term.
+:- import_module mh_substitution.
+:- import_module mh_relation.
+:- import_module mh_proposition.
+:- import_module mh_tuple.
+:- import_module mh_ordered_term_set.
 
 %-----------------------------------------------------------------------------%
 
@@ -33,14 +37,79 @@
 	mh_scope::in, mh_scope::out,
 	mh_term::in, mh_substitution::in, mh_term::out) is det.
 
+:- pred substitute_relation(eval_strategy::in,
+	mh_environment::in, mh_environment::out,
+	mh_scope::in, mh_scope::out,
+	mh_relation::in, mh_substitution::in, mh_relation::out) is det.
+
+:- pred substitute_proposition(eval_strategy::in,
+	mh_environment::in, mh_environment::out,
+	mh_scope::in, mh_scope::out,
+	mh_proposition::in, mh_substitution::in, mh_proposition::out) is det.
+
+:- pred substitute_tuple(eval_strategy::in,
+	mh_environment::in, mh_environment::out,
+	mh_scope::in, mh_scope::out,
+	mh_tuple::in, mh_substitution::in, mh_tuple::out) is det.
+
+:- pred substitute_ordered_term_set(eval_strategy::in,
+	mh_environment::in, mh_environment::out,
+	mh_scope::in, mh_scope::out,
+	mh_ordered_term_set::in, mh_substitution::in, mh_ordered_term_set::out)
+	is det.	
+	
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
 
+:- import_module require.
+
 %-----------------------------------------------------------------------------%
 
-substitute(_, _, !Env, !Scope, !.Term, _, !:Term) :- 
+substitute(Strat, !Env, !Scope, !.Term, Sub, !:Term) :-
+	require_complete_switch [!.Term] (
+		(!.Term = atom(_) ; !.Term = value(_)), !:Term = !.Term
+	;
+		!.Term = var(ID),
+		(if sub_id_search(Sub, ID, Found)
+		then !:Term = Found
+		else true
+		)
+	;
+		!.Term = cons(Car, Cdr),
+		substitute(Strat, !Env, !Scope, Car, Sub, NewCar),
+		substitute_tuple(Strat, !Env, !Scope, Cdr, Sub, NewCdr),
+		(if Car = NewCar, Cdr = NewCdr
+		then true
+		else !:Term = cons(NewCar, NewCdr)
+		)
+	;
+		!.Term = relation(Relation),
+		substitute_relation(Strat, !Env, !Scope, Relation, Sub, NewRelation),
+		(if Relation = NewRelation
+		then true
+		else !:Term = relation(NewRelation)
+		)
+	).
+
+substitute_relation(_, !Env, !Scope, !.Term, _, !:Term) :- 
+	sorry($module, $pred, "substitute_relation/8").
+
+:- pragma no_determinism_warning(substitute_relation/8).
+
+substitute_proposition(_, !Env, !Scope, !.Term, _, !:Term) :- 
+	sorry($module, $pred, "substitute_proposition/8").
+
+:- pragma no_determinism_warning(substitute_proposition/8).
+
+substitute_tuple(_, !Env, !Scope, !.Term, _, !:Term) :- 
+	sorry($module, $pred, "substitute_tuple/8").
+
+:- pragma no_determinism_warning(substitute_tuple/8).
+
+substitute_ordered_term_set(_, !Env, !Scope, !.Term, _, !:Term) :- 
 	sorry($module, $pred, "substitute/8").
 
-:- pragma no_determinism_warning(substitute/8).
+:- pragma no_determinism_warning(substitute_ordered_term_set/8).
+
